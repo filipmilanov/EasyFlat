4a0daefefb769dced9069f23b2813d1abc8f20ff
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-23 16:22:07 +0100

    Revert "[CICD] adds checkstyle validation"
    This reverts commit 42ca9650c670018653b0fa9d2030b49f4664f3ad.

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/SimpleMessageService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/SimpleMessageService.java
@@ -24,14 +24,9 @@ public class SimpleMessageService implements MessageService {
     }
 
     @Override
-  public List<Message> findAll() {
-             LOGGER.debug("Find all messages");
-
-
-
-         return messageRepository.findAllByOrderByPublishedAtDesc();
-
-
+    public List<Message> findAll() {
+        LOGGER.debug("Find all messages");
+        return messageRepository.findAllByOrderByPublishedAtDesc();
     }
 
     @Override
