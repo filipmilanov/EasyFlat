6c5df0de61c06839611b51e471b8487e5aadb92c
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-24 16:11:40 +0100

    [DS-5.1] adds JPARepositories
    Refs: #11

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/DigitalStorageRepository.java
@@ -0,0 +1,8 @@
+package at.ac.tuwien.sepr.groupphase.backend.repository;
+
+import at.ac.tuwien.sepr.groupphase.backend.entity.DigitalStorage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DigitalStorageRepository extends JpaRepository<DigitalStorage, Long> {
+
+}
