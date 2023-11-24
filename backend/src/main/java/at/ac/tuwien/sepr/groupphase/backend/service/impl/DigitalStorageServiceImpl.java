1d70ecc14d48dac2ff644fa7d5dc2fe62d039525
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-24 23:32:59 +0100

    [DS-5.1] implements DigitalStorageServiceImpl with create method
    Refs: #11

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/DigitalStorageServiceImpl.java
@@ -0,0 +1,57 @@
+package at.ac.tuwien.sepr.groupphase.backend.service.impl;
+
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.DigitalStorageDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.DigitalStorageMapper;
+import at.ac.tuwien.sepr.groupphase.backend.entity.DigitalStorage;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Item;
+import at.ac.tuwien.sepr.groupphase.backend.entity.ItemOrderType;
+import at.ac.tuwien.sepr.groupphase.backend.repository.DigitalStorageRepository;
+import at.ac.tuwien.sepr.groupphase.backend.service.DigitalStorageService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DigitalStorageServiceImpl implements DigitalStorageService {
+
+    private final DigitalStorageRepository digitalStorageRepository;
+    private final DigitalStorageMapper digitalStorageMapper;
+
+    public DigitalStorageServiceImpl(DigitalStorageRepository digitalStorageRepository, DigitalStorageMapper digitalStorageMapper) {
+        this.digitalStorageRepository = digitalStorageRepository;
+        this.digitalStorageMapper = digitalStorageMapper;
+    }
+
+    @Override
+    public Optional<DigitalStorage> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Item> findAllItemsOfStorage(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Item> findAllItemsOfStorageOrdered(Long id, ItemOrderType orderType) {
+        return null;
+    }
+
+    @Override
+    public DigitalStorage create(DigitalStorageDto storageDto) {
+        DigitalStorage storage = digitalStorageMapper.dtoToEntity(storageDto);
+
+        return digitalStorageRepository.save(storage);
+    }
+
+    @Override
+    public DigitalStorage update(DigitalStorageDto storage) {
+        return null;
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
+}
