0bb0df39e60997b6d4813cd224582116ada7157a
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-25 00:12:28 +0100

    [DS-5.1] changes dto record to class
    Refs: #11

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/DigitalStorageDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/DigitalStorageDto.java
@@ -1,15 +1,29 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
-public record DigitalStorageDto(
-    Long storId,
-    String title
-) {
+import jakarta.validation.constraints.NotEmpty;
 
-    public DigitalStorageDto withId(Long storId) {
-        return new DigitalStorageDto(
-            storId,
-            title
-        );
+public class DigitalStorageDto {
+
+    private Long storId;
+    @NotEmpty(message = "Title cannot be empty")
+    private String title;
+
+    public DigitalStorageDto() {
     }
 
+    public Long getStorId() {
+        return storId;
+    }
+
+    public void setStorId(Long storId) {
+        this.storId = storId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
