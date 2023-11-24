8d9edc2371df59b74ccea324ee66df95f5adb84a
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-24 23:33:10 +0100

    [DS-5.1] implements rest function for creating
    Refs: #11

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/StorageEndpoint.java
@@ -0,0 +1,30 @@
+package at.ac.tuwien.sepr.groupphase.backend.endpoint;
+
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.DigitalStorageDto;
+import at.ac.tuwien.sepr.groupphase.backend.entity.DigitalStorage;
+import at.ac.tuwien.sepr.groupphase.backend.service.DigitalStorageService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/storage")
+public class StorageEndpoint {
+
+    private final DigitalStorageService digitalStorageService;
+
+    public StorageEndpoint(DigitalStorageService digitalStorageService) {
+        this.digitalStorageService = digitalStorageService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public DigitalStorage create(@RequestBody DigitalStorageDto digitalStorageDto) {
+        return digitalStorageService.create(digitalStorageDto);
+    }
+
+
+}
