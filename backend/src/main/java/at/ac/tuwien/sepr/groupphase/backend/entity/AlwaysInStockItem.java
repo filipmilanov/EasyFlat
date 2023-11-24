5cb803a1f60ec8dc5afa4f1232501f3f460e3654
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-24 15:14:51 +0100

    [DS-5.1] creates entities without relation
    Refs: #11

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/AlwaysInStockItem.java
@@ -0,0 +1,22 @@
+package at.ac.tuwien.sepr.groupphase.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("AlwaysInStock")
+public class AlwaysInStockItem extends Item {
+
+    @Column
+    private Long minimumQuantity;
+
+    public Long getMinimumQuantity() {
+        return minimumQuantity;
+    }
+
+    public void setMinimumQuantity(Long minimumQuantity) {
+        this.minimumQuantity = minimumQuantity;
+    }
+
+}
