ab8088031b27cc149ae84b9b1aa962119297e60b
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-24 23:41:15 +0100

    [DS-5.1] adds security for endpoints
    Refs: #11

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ItemEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ItemEndpoint.java
@@ -4,6 +4,7 @@ import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ItemDto;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Item;
 import at.ac.tuwien.sepr.groupphase.backend.service.ItemService;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class ItemEndpoint {
         this.itemService = itemService;
     }
 
+    @Secured("ROLE_USER")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Item create(@RequestBody ItemDto itemDto) {
