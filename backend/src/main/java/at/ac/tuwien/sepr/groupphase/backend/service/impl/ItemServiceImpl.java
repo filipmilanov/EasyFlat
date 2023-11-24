bc2d212fadad0e6b3356d3adc6efdfb203014145
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-24 22:31:45 +0100

    [DS-5.1] creates ItemService implementation
    Refs: #11

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/ItemServiceImpl.java
@@ -0,0 +1,45 @@
+package at.ac.tuwien.sepr.groupphase.backend.service.impl;
+
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ItemDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.ItemMapper;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Item;
+import at.ac.tuwien.sepr.groupphase.backend.repository.ItemRepository;
+import at.ac.tuwien.sepr.groupphase.backend.service.ItemService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+
+    private final ItemRepository itemRepository;
+    private final ItemMapper itemMapper;
+
+    public ItemServiceImpl(ItemRepository itemRepository, ItemMapper itemMapper) {
+        this.itemRepository = itemRepository;
+        this.itemMapper = itemMapper;
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Item create(ItemDto itemDto) {
+        Item item = itemMapper.itemDtoToItem(itemDto);
+
+        return itemRepository.save(item);
+    }
+
+    @Override
+    public Item update(ItemDto item) {
+        return null;
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
+}
