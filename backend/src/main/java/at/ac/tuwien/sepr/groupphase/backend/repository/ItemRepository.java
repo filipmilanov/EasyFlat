1da0f96e945e36dfc95b772b30a1c35b12a520e2
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-24 22:33:33 +0100

    [DS-5.1] defines save method in ItemRepository
    Refs: #11

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ItemRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ItemRepository.java
@@ -2,6 +2,10 @@ package at.ac.tuwien.sepr.groupphase.backend.repository;
 
 import at.ac.tuwien.sepr.groupphase.backend.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    Item save(Item item);
 }
