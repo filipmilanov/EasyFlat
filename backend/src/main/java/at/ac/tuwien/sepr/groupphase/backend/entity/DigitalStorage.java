6908f096a649b38368bd8906f46955ca2f91e8ac
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-24 22:39:12 +0100

    [DS-5.1] fixes imports in DigitalStorage
    Refs: #11

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/DigitalStorage.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/DigitalStorage.java
@@ -1,6 +1,11 @@
 package at.ac.tuwien.sepr.groupphase.backend.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 import java.util.List;
 import java.util.Objects;
