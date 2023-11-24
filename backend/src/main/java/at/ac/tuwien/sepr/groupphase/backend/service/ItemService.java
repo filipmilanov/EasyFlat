39112ea073b2b6941f8c062e2f6def44e280c9c4
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-24 21:10:19 +0100

    [DS-5.1] defines service as Spring Bean
    Refs: #11

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/ItemService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/ItemService.java
@@ -1,12 +1,14 @@
 package at.ac.tuwien.sepr.groupphase.backend.service;
 
 import at.ac.tuwien.sepr.groupphase.backend.entity.Item;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 /**
  * Service for working with Items.
  */
+@Service
 public interface ItemService {
 
     /**
