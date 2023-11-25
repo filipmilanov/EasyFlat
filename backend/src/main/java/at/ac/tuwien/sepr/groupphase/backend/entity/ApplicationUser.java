7a36ae7ca43eb5cf388c36730ecd8e4b745ccf7d
Radina Grancharova <radina.grancharova@tuwien.ac.at>
2023-11-25 21:58:50 +0200

    [UM-1.2] backend implemented

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/ApplicationUser.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/ApplicationUser.java
@@ -1,11 +1,27 @@
 package at.ac.tuwien.sepr.groupphase.backend.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 //TODO: replace this class with a correct ApplicationUser Entity implementation
+@Entity(name = "application_user")
 public class ApplicationUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String email;
+    @Column
     private String password;
+    @Column
     private Boolean admin;
+
 
     public ApplicationUser() {
     }
@@ -38,5 +54,13 @@ public class ApplicationUser {
 
     public void setAdmin(Boolean admin) {
         this.admin = admin;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/UserRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/UserRepository.java
@@ -1,32 +1,15 @@
 package at.ac.tuwien.sepr.groupphase.backend.repository;
 
 import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 //TODO: replace this class with a correct ApplicationUser JPARepository implementation
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<ApplicationUser, Long> {
 
-    private final ApplicationUser user;
-    private final ApplicationUser admin;
-
-    @Autowired
-    public UserRepository(PasswordEncoder passwordEncoder) {
-        user = new ApplicationUser("user@email.com", passwordEncoder.encode("password"), false);
-        admin = new ApplicationUser("admin@email.com", passwordEncoder.encode("password"), true);
-    }
-
-    public ApplicationUser findUserByEmail(String email) {
-        if (email.equals(user.getEmail())) {
-            return user;
-        }
-        if (email.equals(admin.getEmail())) {
-            return admin;
-        }
-        return null; // In this case null is returned to fake Repository behavior
-    }
-
+    @Query("SELECT u FROM application_user u WHERE u.email = ?1")
+    ApplicationUser findUserByEmail(String email);
 
 }
