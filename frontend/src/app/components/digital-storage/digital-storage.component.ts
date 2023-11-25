6785b665adaca01740c405652985a77b9cf5a302
Ivan Andreev <e12025981@student.tuwien.ac.at>
2023-11-25 09:51:22 +0100

    [DS-5.6] add component for digitalStorage

---
+++ frontend/src/app/components/digital-storage/digital-storage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-digital-storage',
+  templateUrl: './digital-storage.component.html',
+  styleUrls: ['./digital-storage.component.scss']
+})
+export class DigitalStorageComponent {
+
+}
