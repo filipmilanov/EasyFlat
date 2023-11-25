f51ed7523323c4b8a64c6fb5f43c76acdfd108c6
Ivan Andreev <e12025981@student.tuwien.ac.at>
2023-11-25 13:23:28 +0100

    [DS-5.6] new DTO for storageItem.ts Frontend for storage is ready.

--- frontend/src/app/components/digital-storage/digital-storage.component.ts
+++ frontend/src/app/components/digital-storage/digital-storage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {storageItemList} from "../../dtos/storageItem";
 
 @Component({
   selector: 'app-digital-storage',
@@ -7,5 +8,34 @@ import { Component } from '@angular/core';
 })
 
 export class DigitalStorageComponent {
+  title = "Test Title"
+  itemQuantity = 50
+  itemExpirationDate  = "Test Date"
+  items: storageItemList[] = [];
 
+
+  constructor() {
+    // Initialize the items array with test items
+    this.initializeTestItems();
+  }
+
+
+  private initializeTestItems() {
+    // Add test items to the items array
+    this.items.push({
+      title: 'Item 1',
+      quantity: 10,
+      maxQuantity: 100,
+      expirationDate: '2023-12-31'
+    });
+
+    this.items.push({
+      title: 'Item 2',
+      quantity: 20,
+      maxQuantity: 200,
+      expirationDate: '2023-11-30'
+    });
+
+    // Add more test items as needed
+  }
 }
---
+++ frontend/src/app/dtos/storageItem.ts
@@ -0,0 +1,6 @@
+export class storageItemList {
+  title:string;
+  quantity:number;
+  maxQuantity:number;
+  expirationDate:string;
+}
