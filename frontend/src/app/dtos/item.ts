811ecadeb5b3c441feb913d24ad6e34a811e024b
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-26 19:36:42 +0100

    [DS-5.1] updates itemDto
    Refs: #11

--- frontend/src/app/dtos/item.ts
+++ frontend/src/app/dtos/item.ts
@@ -1,13 +1,14 @@
-export class Item {
-  itemId: number;
-  ean: string;
-  generalName: string;
-  productName: string;
-  brand: string;
-  quantityCurrent: number;
-  quantityTotal: number;
-  unit: string;
-  expireDate: Date;
-  description: string;
-  priceInCent: number;
+export class ItemDto {
+  itemId?: number;
+  ean?: string;
+  generalName?: string;
+  productName?: string;
+  brand?: string;
+  quantityCurrent?: number;
+  quantityTotal?: number;
+  unit?: string;
+  expireDate?: Date;
+  description?: string;
+  priceInCent?: number;
+  ingredients?: [string];
 }
