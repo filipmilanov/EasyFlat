0467f5da8a0e0f9eaf5532e8c409a2cd8921b19d
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-26 19:36:58 +0100

    [DS-5.1] implements add ingredient
    Refs: #11

--- frontend/src/app/components/digital-storage/item-create/item-create.component.ts
+++ frontend/src/app/components/digital-storage/item-create/item-create.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ItemDto} from "../../../dtos/item";
 
 @Component({
   selector: 'app-item-create',
   templateUrl: './item-create.component.html',
   styleUrls: ['./item-create.component.scss']
 })
-export class ItemCreateComponent {
+export class ItemCreateComponent implements OnInit{
+
+  item: ItemDto = {}
+  isCreateMode: boolean;
+
+  ngOnInit(): void {
+
+  }
+
+  submit(): void {
+
+  }
+
+  addIngredient(ingredient: string): void {
+    if (ingredient == undefined || ingredient.length == 0) {
+      return
+    }
+    if (this.item.ingredients === undefined) {
+      this.item.ingredients = [ingredient];
+    } else {
+      this.item.ingredients.push(ingredient);
+    }
+  }
+
+
+
+
 
 }
