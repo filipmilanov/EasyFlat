b6684a32ef6d1a73250d1491c53f594666db6737
Ivan Andreev <e12025981@student.tuwien.ac.at>
2023-11-26 11:01:19 +0100

    [DS-5.6] Add itemListDto checkstyle fix

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/itemListDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/itemListDto.java
@@ -2,10 +2,6 @@ package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
 import java.time.LocalDate;
 
-public record itemListDto(Long itemId,
-                          String productName,
-                          String brand,
-                          Long quantityCurrent,
-                          Long quantityTotal,
+public record ItemListDto(Long itemId, String productName, String brand, Long quantityCurrent, Long quantityTotal,
                           LocalDate expireDate) {
 }
