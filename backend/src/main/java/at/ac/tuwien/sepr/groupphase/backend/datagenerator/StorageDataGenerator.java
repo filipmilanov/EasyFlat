06bfccd3e40f11bd537b4d24b4d6e2ace69c8480
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-27 10:08:18 +0100

    [TestSuit] feat generate testdata for digital storage
    Refs: #88

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/StorageDataGenerator.java
@@ -0,0 +1,34 @@
+package at.ac.tuwien.sepr.groupphase.backend.datagenerator;
+
+import at.ac.tuwien.sepr.groupphase.backend.entity.DigitalStorage;
+import at.ac.tuwien.sepr.groupphase.backend.repository.DigitalStorageRepository;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.lang.invoke.MethodHandles;
+
+@Profile("generateData")
+@Component
+public class StorageDataGenerator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final int NUMBER_OF_MESSAGES_TO_GENERATE = 5;
+    private final DigitalStorageRepository digitalStorageRepository;
+
+    public StorageDataGenerator(DigitalStorageRepository digitalStorageRepository) {
+        this.digitalStorageRepository = digitalStorageRepository;
+    }
+
+    @PostConstruct
+    private void generateDigitalStorages() {
+        LOGGER.debug("generating {} message entries", NUMBER_OF_MESSAGES_TO_GENERATE);
+        for (int i = 0; i < NUMBER_OF_MESSAGES_TO_GENERATE; i++) {
+            DigitalStorage message = new DigitalStorage();
+            message.setTitle("Storage " + (i + 1));
+            LOGGER.debug("saving message {}", message);
+            digitalStorageRepository.save(message);
+        }
+    }
+}
