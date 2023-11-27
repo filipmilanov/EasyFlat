364c5a700c51a3b9d518c9b6ab5f549468f8b042
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-27 17:27:32 +0100

    [DS-5.1] fix correct validation constraint
    Refs: #11

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/AlwaysInStockItem.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/AlwaysInStockItem.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @DiscriminatorValue("AlwaysInStock")
@@ -12,7 +12,7 @@ public class AlwaysInStockItem extends Item {
 
     @Column
     @Min(value = 0, message = "The minimum quantity must be positive")
-    @NotEmpty(message = "The minimum quantity cannot be empty")
+    @NotNull(message = "The minimum quantity cannot be empty")
     private Long minimumQuantity;
 
     public Long getMinimumQuantity() {
