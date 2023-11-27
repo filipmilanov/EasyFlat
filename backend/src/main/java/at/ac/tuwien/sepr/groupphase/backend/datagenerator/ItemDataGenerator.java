14709a2169199cbdd514c5286d23301c042438d7
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-27 10:13:18 +0100

    [TestSuit] feat generate testdata for item
    Refs: #88

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/ItemDataGenerator.java
@@ -0,0 +1,62 @@
+package at.ac.tuwien.sepr.groupphase.backend.datagenerator;
+
+import at.ac.tuwien.sepr.groupphase.backend.entity.DigitalStorage;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Ingredient;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Item;
+import at.ac.tuwien.sepr.groupphase.backend.repository.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.lang.invoke.MethodHandles;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Profile("generateData")
+@Component
+public class ItemDataGenerator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final int NUMBER_OF_ENTITIES_TO_GENERATE = 5;
+    private final ItemRepository itemRepository;
+
+    public ItemDataGenerator(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    @PostConstruct
+    private void generateDigitalStorages() {
+        LOGGER.debug("generating {} message entries", NUMBER_OF_ENTITIES_TO_GENERATE);
+        for (int i = 0; i < NUMBER_OF_ENTITIES_TO_GENERATE; i++) {
+            Item item = new Item();
+            item.setGeneralName("Item" + (i + 1));
+            item.setEan("123456789012" + i);  // Replace with valid EAN numbers
+            item.setProductName("Test Product " + (i + 1));
+            item.setBrand("Test Brand " + (i + 1));
+            item.setQuantityCurrent(10L + i);
+            item.setQuantityTotal(20L + i);
+            item.setUnit("pieces");
+            item.setExpireDate(LocalDate.now().plusMonths(i + 1));  // Set expire date to current date + i months
+            item.setDescription("This is a test product description for Item " + (i + 1));
+            item.setPriceInCent(500L + i * 100);
+
+            DigitalStorage storage = new DigitalStorage();
+            storage.setStorId(1L);
+            item.setStorage(storage);
+
+            List<Ingredient> ingredientList = new ArrayList<>();
+            Ingredient ingredient1 = new Ingredient();
+            ingredient1.setIngrId(1L);
+            Ingredient ingredient2 = new Ingredient();
+            ingredient2.setIngrId(2L);
+            ingredientList.add(ingredient1);
+            ingredientList.add(ingredient2);
+            item.setIngredientList(ingredientList);
+
+            LOGGER.debug("saving item {}", item);
+            itemRepository.save(item);
+        }
+    }
+}
