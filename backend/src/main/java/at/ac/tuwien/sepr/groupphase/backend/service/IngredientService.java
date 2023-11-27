a0aa1f96a2106cbca0a0c9a1415a8f5a6fc2a875
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-27 11:06:51 +0100

    [DS-5.1] implements findAllByIds of ingredients
    Refs: #11

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/IngredientService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/IngredientService.java
@@ -1,7 +1,17 @@
 package at.ac.tuwien.sepr.groupphase.backend.service;
 
-import org.springframework.stereotype.Service;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Ingredient;
 
-@Service
+import java.util.List;
+
+
 public interface IngredientService {
+
+    /**
+     * Search for all Ingredients which has a given id.
+     *
+     * @param ids a list of Ids
+     * @return a list of all ingredients with a given id
+     */
+    List<Ingredient> findAllByIds(List<Long> ids);
 }
