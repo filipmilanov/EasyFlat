9a6eb9ff8959d85b0ce635e48ef29412d0a73d9b
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-27 10:46:49 +0100

    [DS-5.1] fix itemMapper
    Refs: #11

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/ItemMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/ItemMapper.java
@@ -3,15 +3,16 @@ package at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ItemDto;
 import at.ac.tuwien.sepr.groupphase.backend.entity.DigitalStorage;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Item;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper
 public interface ItemMapper {
 
-    @Mapping(target = "digitalStorage", source = "digitalStorage")
-    @Mapping(target = " ingredientList", ignore = true)
-    Item dtoToItem(ItemDto itemDto, DigitalStorage digitalStorage);
+    @Mapping(target = "storage", expression = "java( persistedDigitalStorage )")
+    @Mapping(target = "ingredientList", ignore = true)
+    Item dtoToItem(ItemDto itemDto, @Context DigitalStorage persistedDigitalStorage);
 
     ItemDto itemToDto(Item item);
 }
