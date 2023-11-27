f50ef7116cb7d1609ca6b887a805b2cc7e931de7
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-27 21:20:43 +0100

    [DS-5.1] adds connection from detail view to create view
    Refs: #11

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import {NgModule} from '@angular/core';
-import {mapToCanActivate, RouterModule, Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
-import {AuthGuard} from './guards/auth.guard';
-import {MessageComponent} from './components/message/message.component';
 import {DigitalStorageComponent} from "./components/digital-storage/digital-storage.component";
 import {ItemCreateComponent} from "./components/digital-storage/item-create/item-create.component";
 import {ItemDetailComponent} from "./components/digital-storage/item-detail/item-detail.component";
@@ -11,11 +9,10 @@ import {ItemDetailComponent} from "./components/digital-storage/item-detail/item
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'message', canActivate: mapToCanActivate([AuthGuard]), component: MessageComponent},
   {path: 'digital-storage', children: [
       {path: '', component: DigitalStorageComponent},
       {path: 'item', children: [
-          {path: 'new', component: ItemCreateComponent},
+          {path: 'create', component: ItemCreateComponent},
           {path: ':id/detail', component: ItemDetailComponent}
         ]}
   ]}
