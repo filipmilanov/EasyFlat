3901afd6dd17122e36abf5e8778851e27375b035
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-27 10:15:12 +0100

    [TestSuit] feat generate testdata for ingredient
    Refs: #88

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/IngredientsDataGenerator.java
@@ -0,0 +1,36 @@
+package at.ac.tuwien.sepr.groupphase.backend.datagenerator;
+
+import at.ac.tuwien.sepr.groupphase.backend.entity.Ingredient;
+import at.ac.tuwien.sepr.groupphase.backend.repository.IngredientRepository;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.lang.invoke.MethodHandles;
+
+@Profile("generateData")
+@Component
+public class IngredientsDataGenerator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final int NUMBER_OF_ENTITIES_TO_GENERATE = 5;
+    private final IngredientRepository ingredientRepository;
+
+    public IngredientsDataGenerator(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @PostConstruct
+    private void generateDigitalStorages() {
+        LOGGER.debug("generating {} message entries", NUMBER_OF_ENTITIES_TO_GENERATE);
+        for (int i = 0; i < NUMBER_OF_ENTITIES_TO_GENERATE; i++) {
+
+            Ingredient ingredient1 = new Ingredient();
+            ingredient1.setTitle("Ingredient " + (i + 1));
+
+            LOGGER.debug("saving item {}", ingredient1);
+            ingredientRepository.save(ingredient1);
+        }
+    }
+}
