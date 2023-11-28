13e334a18c0c555ebadc1b61643fa35832362efa
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-28 15:55:04 +0100

    [DS-5.1] adds single object mapper
    Refs: #11

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/IngredientMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/IngredientMapper.java
@@ -10,7 +10,9 @@ import java.util.List;
 @Mapper
 public abstract class IngredientMapper {
 
+    public abstract List<Ingredient> dtoListToEntityList(List<IngredientDto> ingredientDtos);
+
     @Mapping(target = "ingrId", source = "ingredientId")
     @Mapping(target = "title", source = "name")
-    public abstract List<Ingredient> dtoListToEntityList(List<IngredientDto> ingredientDtos);
+    public abstract Ingredient ingredientDtoToIngredient(IngredientDto ingredientDto);
 }
