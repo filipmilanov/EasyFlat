098df315c3d8a792e10f5a6c462ed2bc691bdef6
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-11-28 15:18:19 +0100

    [DS-5.1] creates IngredientMapper
    Refs: #11

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/IngredientMapper.java
@@ -0,0 +1,16 @@
+package at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper;
+
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.IngredientDto;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Ingredient;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper
+public abstract class IngredientMapper {
+
+    @Mapping(target = "ingrId", source = "ingredientId")
+    @Mapping(target = "title", source = "name")
+    public abstract List<Ingredient> dtoListToEntityList(List<IngredientDto> ingredientDtos);
+}
