c7c3389dc4eedacd7cb9dc63eb9f231398317668
Philipp Schoeller <e11907084@student.tuwien.ac.at>
2023-11-29 12:25:11 +0100

    [DS-5.2] transform create component into create-edit component
    support create and edit mode in the component.ts
    adapt the template to display correct text according to mode
    add route for edit mode
    add ability to go to edit mode from detail view
    
    Refs: #21

--- frontend/src/app/components/digital-storage/item-create/item-create.component.ts
+++ frontend/src/app/components/digital-storage/item-create/item-create.component.ts
@@ -4,52 +4,134 @@ import {NgForm} from "@angular/forms";
 import {ItemService} from "../../../services/item.service";
 import {ToastrService} from "ngx-toastr";
 import {DigitalStorageDto} from "../../../dtos/digitalStorageDto";
-import {of} from "rxjs";
+import {Observable, of} from "rxjs";
 import {StorageService} from "../../../services/storage.service";
+import {ActivatedRoute, Router} from "@angular/router";
+
+export enum ItemCreateEditMode {
+  create,
+  edit,
+}
 
 @Component({
-  selector: 'app-item-create',
+  selector: 'app-item-create-edit',
   templateUrl: './item-create.component.html',
   styleUrls: ['./item-create.component.scss']
 })
 export class ItemCreateComponent implements OnInit{
 
+  mode: ItemCreateEditMode = ItemCreateEditMode.create;
   item: ItemDto = {
     alwaysInStock: false,
     addToFiance: false
   }
-  isCreateMode: boolean;
   priceInEuro: number = 0.00;
 
 
   constructor(
     private itemService: ItemService,
-    private notification: ToastrService,
     private storageService: StorageService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private notification: ToastrService,
   ) {
   }
 
-  ngOnInit(): void {
-
+  public get heading(): string {
+    switch (this.mode) {
+      case ItemCreateEditMode.create:
+        return 'Create New Item';
+      case ItemCreateEditMode.edit:
+        return 'Edit Item';
+      default:
+        return '?';
+    }
   }
 
-  submit(form: NgForm): void {
+  public get submitButtonText(): string {
+    switch (this.mode) {
+      case ItemCreateEditMode.create:
+        return 'Create';
+      case ItemCreateEditMode.edit:
+        return 'Update';
+      default:
+        return '?';
+    }
+  }
+
+  get modeIsCreate(): boolean {
+    return this.mode === ItemCreateEditMode.create;
+  }
+
+  private get modeActionFinished(): string {
+    switch (this.mode) {
+      case ItemCreateEditMode.create:
+        return 'created';
+      case ItemCreateEditMode.edit:
+        return 'updated';
+      default:
+        return '?';
+    }
+  }
+
+  ngOnInit(): void {
+    this.route.data.subscribe(data => {
+      this.mode = data.mode;
+    });
+
+    if (this.mode === ItemCreateEditMode.edit) {
+      this.route.params.subscribe({
+        next: params => {
+          const itemId = params.id;
+          this.itemService.getById(itemId).subscribe({
+            next: res => {
+              this.item = res;
+            },
+            error: error => {
+              console.error(`Item could not be retrieved from the backend: ${error}`);
+              this.router.navigate(['/digital-storage']);
+              this.notification.error('Item could not be retrieved', "Error");
+            }
+          })
+        },
+        error: error => {
+          console.error(`Item could not be retrieved using the ID from the URL: ${error}`);
+          this.router.navigate(['/digital-storage']);
+          this.notification.error('No item provided for editing', "Error");
+        }
+      })
+    }
+  }
+
+  public onSubmit(form: NgForm): void {
     this.item.priceInCent = this.priceInEuro * 100;
     this.item.quantityCurrent = this.item.quantityTotal;
+    console.log('is form valid?', form.valid, this.item);
 
-    let o = this.itemService.createItem(this.item);
-
-    o.subscribe({
-      next: res => {
-        this.notification.success(this.item.productName + " was added to your storage", "Success");
-        form.reset();
-      },
-      error: err => {
-        console.error("Error adding item to storage:", err);
-        this.notification.error(err.error, "Error");
+    if (form.valid) {
+      let observable: Observable<ItemDto>;
+      switch (this.mode) {
+        case ItemCreateEditMode.create:
+          observable = this.itemService.createItem(this.item);
+          break;
+        case ItemCreateEditMode.edit:
+          observable = this.itemService.updateItem(this.item);
+          break;
+        default:
+          console.error('Unknown ItemCreateEditMode', this.mode);
+          return;
       }
-    })
-
+      observable.subscribe({
+        next: data => {
+          this.notification.success(`Item ${this.item.productName} successfully ${this.modeActionFinished} and added to the storage.`, "Success");
+          this.router.navigate(['/digital-storage']);
+        },
+        error: error => {
+          console.error(`Error creating item ${error}`);
+          this.notification.error(`Item could not be ${this.modeActionFinished} and was not added to storage.`, "Error");
+        }
+      });
+    }
   }
 
   addIngredient(ingredient: string): void {
