4bffe23a398e9407f2a8513d8837b6f88df85f0e
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-08 10:43:20 +0100

    feat: creates a general Authenticator
    Refs: #183

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/Authenticator/Authenticator.java
@@ -0,0 +1,44 @@
+package at.ac.tuwien.sepr.groupphase.backend.service.impl.Authenticator;
+
+import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
+import at.ac.tuwien.sepr.groupphase.backend.exception.AuthenticationException;
+import at.ac.tuwien.sepr.groupphase.backend.service.impl.CustomUserDetailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
+@Component
+public class Authenticator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private final CustomUserDetailService customUserDetailService;
+
+    public Authenticator(CustomUserDetailService customUserDetailService) {
+        this.customUserDetailService = customUserDetailService;
+    }
+
+    public void authenticateUser(String jwt, List<Long> allowedUser, String errorMessage) throws AuthenticationException {
+        LOGGER.trace("authenticateUser({}, {}, {})", jwt, id, errorMessage);
+
+        ApplicationUser user = customUserDetailService.getUser(jwt);
+        if (user == null) {
+            throw new AuthenticationException("Authentication failed", List.of("User does not exists"));
+        }
+
+        if (!allowedUser.contains(user.getId())) {
+            throw new AuthenticationException("Authentication failed", List.of(errorMessage));
+        }
+    }
+
+    public void authenticateUser(String jwt, List<Long> id) throws AuthenticationException {
+        LOGGER.trace("authenticateUser({}, {})", jwt, id);
+
+        authenticateUser(jwt, id, "User does not have access to this resource");
+    }
+}
