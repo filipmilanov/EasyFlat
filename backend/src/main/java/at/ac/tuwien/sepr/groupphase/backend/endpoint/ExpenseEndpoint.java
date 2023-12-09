1fbdaa7552cae593813cc92be53cf1a13eac21c5
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-10 00:27:07 +0100

    feat: adds findById function in endpoint
    Refs: #196

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ExpenseEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ExpenseEndpoint.java
@@ -1,7 +1,13 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint;
 
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ExpenseDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.ExpenseMapper;
+import at.ac.tuwien.sepr.groupphase.backend.service.ExpenseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +18,22 @@ import java.lang.invoke.MethodHandles;
 public class ExpenseEndpoint {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private final ExpenseService expenseService;
+    private final ExpenseMapper expenseMapper;
+
+    public ExpenseEndpoint(ExpenseService expenseService, ExpenseMapper expenseMapper) {
+        this.expenseService = expenseService;
+        this.expenseMapper = expenseMapper;
+    }
+
+    @GetMapping("{id}")
+    public ExpenseDto findById(@PathVariable("id") Long id, @RequestHeader("Authorization") String jwt) {
+        LOGGER.info("findById: {}", id);
+
+        return expenseMapper.entityToExpenseDto(
+            expenseService.findById(id, jwt)
+        );
+    }
 
 }
