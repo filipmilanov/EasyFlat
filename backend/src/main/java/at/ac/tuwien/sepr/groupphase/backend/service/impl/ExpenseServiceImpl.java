32ad79225885c201c273c4d1a257bb494b9a3abb
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-09 18:49:47 +0100

    feat: created method declarations for create
    Refs: #196

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/ExpenseServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/ExpenseServiceImpl.java
@@ -1,5 +1,10 @@
 package at.ac.tuwien.sepr.groupphase.backend.service.impl;
 
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ExpenseDto;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Expense;
+import at.ac.tuwien.sepr.groupphase.backend.exception.ConflictException;
+import at.ac.tuwien.sepr.groupphase.backend.exception.NotFoundException;
+import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
 import at.ac.tuwien.sepr.groupphase.backend.service.ExpenseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,4 +15,14 @@ import java.lang.invoke.MethodHandles;
 @Service
 public class ExpenseServiceImpl implements ExpenseService {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Override
+    public Expense findById(Long id) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public Expense create(ExpenseDto expenseDto) throws ValidationException, ConflictException {
+        return null;
+    }
 }
