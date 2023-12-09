78b3c98ca4ac28b1a6cbd6c5e43c2753fa51bfe8
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-10 00:28:28 +0100

    feat: adds findById function in service
    Refs: #196

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/ExpenseServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/ExpenseServiceImpl.java
@@ -5,6 +5,7 @@ import at.ac.tuwien.sepr.groupphase.backend.entity.Expense;
 import at.ac.tuwien.sepr.groupphase.backend.exception.ConflictException;
 import at.ac.tuwien.sepr.groupphase.backend.exception.NotFoundException;
 import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
+import at.ac.tuwien.sepr.groupphase.backend.repository.ExpenseRepository;
 import at.ac.tuwien.sepr.groupphase.backend.service.ExpenseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,9 +17,20 @@ import java.lang.invoke.MethodHandles;
 public class ExpenseServiceImpl implements ExpenseService {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    private final ExpenseRepository expenseRepository;
+
+    public ExpenseServiceImpl(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
     @Override
     public Expense findById(Long id, String jwt) throws NotFoundException {
-        return null;
+        LOGGER.info("findById: {}", id);
+
+        // TODO: authentication
+
+        return expenseRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("Expense not found"));
     }
 
     @Override
