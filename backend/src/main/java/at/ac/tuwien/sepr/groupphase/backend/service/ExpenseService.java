50e60331d76e599a61612463e62e756c1f3f7b19
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-09 18:49:47 +0100

    feat: created method declarations for create
    Refs: #196

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/ExpenseService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/ExpenseService.java
@@ -1,5 +1,30 @@
 package at.ac.tuwien.sepr.groupphase.backend.service;
 
 
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ExpenseDto;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Expense;
+import at.ac.tuwien.sepr.groupphase.backend.exception.ConflictException;
+import at.ac.tuwien.sepr.groupphase.backend.exception.NotFoundException;
+import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
+
 public interface ExpenseService {
+
+    /**
+     * Finds an expense by id.
+     *
+     * @param id the id of the expense
+     * @return the expense if found
+     * @throws NotFoundException if the expense is not persisted
+     */
+    Expense findById(Long id) throws NotFoundException;
+
+    /**
+     * Creates a new expense.
+     *
+     * @param expenseDto the expense to create
+     * @return the created expense with id
+     * @throws ValidationException if the expense is not valid
+     * @throws ConflictException   if the expense would produce an inconsistent state in the database
+     */
+    Expense create(ExpenseDto expenseDto) throws ValidationException, ConflictException;
 }
