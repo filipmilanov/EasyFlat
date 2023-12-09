1cb5830f3066ae3a128747ec1e58a374273b8977
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-09 20:08:12 +0100

    feat: updates dtos
    Refs: #196

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/DebitDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/DebitDto.java
@@ -1,7 +1,15 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
+import at.ac.tuwien.sepr.groupphase.backend.entity.SplitBy;
+import io.soabase.recordbuilder.core.RecordBuilder;
+
+/**
+ * Data transfer object for debits which is used for all splitBy methods.
+ */
+@RecordBuilder
 public record DebitDto(
     UserDetailDto user,
-    Long percentage
+    SplitBy splitBy,
+    Long value
 ) {
 }
