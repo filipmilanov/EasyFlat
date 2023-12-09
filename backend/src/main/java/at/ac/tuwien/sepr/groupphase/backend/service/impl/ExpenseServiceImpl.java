9f7cbb2c1d1364afcde2a76867e7faefbd6cf4cc
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-09 15:32:55 +0100

    feat: added expense service layer classes
    Refs: #205

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/ExpenseServiceImpl.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.sepr.groupphase.backend.service.impl;
+
+import at.ac.tuwien.sepr.groupphase.backend.service.ExpenseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.lang.invoke.MethodHandles;
+
+@Service
+public class ExpenseServiceImpl implements ExpenseService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+}
---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/validator/ExpenseValidator.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.sepr.groupphase.backend.service.impl.validator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.lang.invoke.MethodHandles;
+
+@Component
+public class ExpenseValidator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+}
