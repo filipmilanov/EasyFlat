62fcad756652dc7ba81fa1d36b1d442a86d1a2c4
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-09 15:04:21 +0100

    feat: created basic fields for expense
    Refs: #205

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/Expense.java
@@ -0,0 +1,28 @@
+package at.ac.tuwien.sepr.groupphase.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private SplitBy splitBy;
+
+    @Column
+    private Long amountInCents;
+
+}
