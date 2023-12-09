9281e4237363af4f20f194d583b881adbd3da421
Radina Grancharova <radina.grancharova@tuwien.ac.at>
2023-12-10 01:54:11 +0200

    [KV-7.2] editing recipe fully implemented

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/CookingEndPoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/CookingEndPoint.java
@@ -11,11 +11,13 @@ import jakarta.annotation.security.PermitAll;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/v1/cooking")
@@ -45,6 +47,19 @@ public class CookingEndPoint {
     @PostMapping("/cookbook")
     public RecipeSuggestionDto createCookbookRecipe(@RequestBody RecipeSuggestionDto recipe) throws ConflictException {
         return recipeMapper.entityToRecipeSuggestionDto(cookingService.createCookbookRecipe(recipe));
+    }
+
+    @PermitAll
+    @GetMapping("/cookbook/{id}")
+    public Optional<RecipeSuggestionDto> getCookbookRecipe(@PathVariable Long id) {
+        Optional<RecipeSuggestion> recipe = cookingService.getCookbookRecipe(id);
+        return recipe.flatMap(currentRecipe -> Optional.ofNullable(recipeMapper.entityToRecipeSuggestionDto(currentRecipe)));
+    }
+
+    @PermitAll
+    @PutMapping("/cookbook/{id}")
+    public RecipeSuggestionDto updateCookbookRecipe(@PathVariable Long id, @RequestBody RecipeSuggestionDto recipe) throws ConflictException {
+        return recipeMapper.entityToRecipeSuggestionDto(cookingService.updateCookbookRecipe(recipe.withId(id)));
     }
 
     @PermitAll
