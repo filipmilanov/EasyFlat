c5ac81119cbd0fc430cf9984170a9cf3664feeb9
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-09 18:24:54 +0100

    fix(checkstyle): added brackets to if
    Refs: #205

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/DebitKey.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/DebitKey.java
@@ -36,8 +36,12 @@ public class DebitKey implements Serializable {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         DebitKey debitKey = (DebitKey) o;
         return Objects.equals(expense, debitKey.expense) && Objects.equals(user, debitKey.user);
     }
