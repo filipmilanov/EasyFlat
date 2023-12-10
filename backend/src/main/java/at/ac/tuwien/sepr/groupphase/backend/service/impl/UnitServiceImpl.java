d181cd0f0425c92ef432b7b7b731f1152572e5d9
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-10 13:39:58 +0100

    feat: impl. findAll
    Refs: #208

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/UnitServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/UnitServiceImpl.java
@@ -1,17 +1,30 @@
 package at.ac.tuwien.sepr.groupphase.backend.service.impl;
 
 import at.ac.tuwien.sepr.groupphase.backend.entity.Unit;
+import at.ac.tuwien.sepr.groupphase.backend.repository.UnitRepository;
 import at.ac.tuwien.sepr.groupphase.backend.service.UnitService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.lang.invoke.MethodHandles;
 import java.util.List;
 
 @Service
 public class UnitServiceImpl implements UnitService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private final UnitRepository unitRepository;
+
+    public UnitServiceImpl(UnitRepository unitRepository) {
+        this.unitRepository = unitRepository;
+    }
 
     @Override
     public List<Unit> findAll() {
-        return null;
+        LOGGER.info("findAll()");
+
+        return unitRepository.findAll();
     }
 
     @Override
