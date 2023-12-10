85eb5bb9a94ff706f2dcac66f66a5ab8f7c1dcd6
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-10 15:34:23 +0100

    feat: do not show subunit tree
    Refs: #208

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/UnitMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/UnitMapper.java
@@ -4,12 +4,15 @@ package at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UnitDto;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Unit;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper
 public abstract class UnitMapper {
 
+    @Mapping(target = "subUnit", ignore = true)
+    @Mapping(target = "convertFactor", ignore = true)
     public abstract UnitDto entityToUnitDto(Unit unit);
 
     public abstract Unit unitDtoToEntity(UnitDto unitDto);
