4983ba4cddd0e4b35db5fd73c9248167743e165f
Philipp Schoeller <e11907084@student.tuwien.ac.at>
2023-12-11 20:21:26 +0100

    [BS-6.2] feedback: add OpenFoodFactsService & mapper for API response
    rename ItemFromApiDto to OpenFoodFactsItemDto
    remove status and statusText fields from dto
    rename findItemByEan to findByEan
    add OpenFoodFactsService interface
    add OpenFoodFactsServiceImpl implementation
    add mapper to map API response to a OpenFoodFactsItemDto
    
    Refs: #102

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/ItemServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/ItemServiceImpl.java
@@ -1,7 +1,6 @@
 package at.ac.tuwien.sepr.groupphase.backend.service.impl;
 
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ItemDto;
-import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ItemFromApiDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.ItemMapper;
 import at.ac.tuwien.sepr.groupphase.backend.entity.DigitalStorage;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Ingredient;
@@ -16,26 +15,16 @@ import at.ac.tuwien.sepr.groupphase.backend.service.DigitalStorageService;
 import at.ac.tuwien.sepr.groupphase.backend.service.IngredientService;
 import at.ac.tuwien.sepr.groupphase.backend.service.ItemService;
 import at.ac.tuwien.sepr.groupphase.backend.service.impl.validator.ItemValidator;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -49,15 +38,11 @@ public class ItemServiceImpl implements ItemService {
     private final ItemValidator itemValidator;
     private final ItemStatsRepository itemStatsRepository;
     private final Validator validator;
-    private final RestTemplate restTemplate;
-    private final ObjectMapper objectMapper;
-    private final String openFoodFactsApi = "https://world.openfoodfacts.net/api/v2/product/";
 
     public ItemServiceImpl(ItemRepository itemRepository, DigitalStorageService digitalStorageService,
                            IngredientService ingredientService, ItemMapper itemMapper,
                            ItemValidator itemValidator, Validator validator,
-                           ItemStatsRepository itemStatsRepository, RestTemplate restTemplate,
-                           ObjectMapper objectMapper) {
+                           ItemStatsRepository itemStatsRepository) {
         this.itemRepository = itemRepository;
         this.digitalStorageService = digitalStorageService;
         this.ingredientService = ingredientService;
@@ -65,8 +50,6 @@ public class ItemServiceImpl implements ItemService {
         this.itemValidator = itemValidator;
         this.itemStatsRepository = itemStatsRepository;
         this.validator = validator;
-        this.restTemplate = restTemplate;
-        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -149,105 +132,5 @@ public class ItemServiceImpl implements ItemService {
         LOGGER.trace("delete({})", id);
 
         itemRepository.deleteById(id);
-    }
-
-    @SuppressWarnings("checkstyle:Indentation")
-    @Override
-    public ItemFromApiDto findItemByEan(Long ean) throws ConflictException {
-
-        LOGGER.trace("findItemByEan({})", ean);
-
-        String requestString = openFoodFactsApi + ean;
-
-        try {
-            // Get data from API using EAN code
-            String jsonResponse = restTemplate.getForObject(requestString, String.class);
-
-            // Use JsonNode to get the values we need
-            JsonNode rootNode = objectMapper.readTree(jsonResponse);
-
-            String eanCode = rootNode.path("code").asText();
-
-            String generalName = rootNode.path("product").path("generic_name_en").asText().toLowerCase();
-
-            String productName = rootNode.path("product").path("product_name_en").asText();
-
-            String brand = rootNode.path("product").path("brands").asText();
-
-            Long totalQuantity = rootNode.path("product").path("product_quantity").asLong();
-
-            String unit = rootNode.path("product").path("ecoscore_data").path("adjustments").path("packaging").path("packagings").get(0).path("quantity_per_unit_unit").asText();
-
-            String description = rootNode.path("product").path("category_properties").path("ciqual_food_name:en").asText();
-
-            String boughtAt = rootNode.path("product").path("stores").asText();
-
-            String ingredientList = rootNode.path("product").path("ingredients_text_en").asText();
-
-            List<IngredientDto> ingredientDtoList = Arrays.stream(ingredientList.split(","))
-                .map(String::trim)
-                .map(ingredientName -> IngredientDtoBuilder.builder()
-                    .name(ingredientName)
-                    .build())
-                .toList();
-
-            List<Ingredient> ingredients = findIngredientsAndCreateMissing(ingredientDtoList);
-
-            Long status = rootNode.path("status").asLong();
-
-            String statusText = rootNode.path("status_verbose").asText();
-
-            return new ItemFromApiDto(
-                eanCode,
-                !Objects.equals(generalName, "") ? generalName : productName.toLowerCase(),
-                productName,
-                brand,
-                totalQuantity,
-                unit,
-                description,
-                boughtAt,
-                ingredients,
-                status,
-                statusText
-            );
-
-        } catch (HttpClientErrorException e) {
-            LOGGER.error("Client error while fetching item by EAN: {}", ean);
-        } catch (HttpServerErrorException e) {
-            LOGGER.error("Server error while fetching item by EAN: {}", ean);
-        } catch (ResourceAccessException e) {
-            LOGGER.error("Connection error while fetching item by EAN: {}", ean);
-        } catch (RestClientException e) {
-            LOGGER.error("Unknown error while fetching item by EAN: {}", ean);
-        } catch (IOException e) {
-            LOGGER.error("Error parsing JSON response for item with EAN: {}", ean);
-        }
-
-        return null;
-    }
-
-    private List<Ingredient> findIngredientsAndCreateMissing(List<IngredientDto> ingredientDtoList) throws ConflictException {
-        if (ingredientDtoList == null) {
-            return List.of();
-        }
-        List<Ingredient> ingredientList = ingredientService.findByTitle(
-            ingredientDtoList.stream()
-                .map(IngredientDto::name)
-                .toList()
-        );
-
-        List<IngredientDto> missingIngredients = ingredientDtoList.stream()
-            .filter(ingredientDto ->
-                ingredientList.stream()
-                    .noneMatch(ingredient ->
-                        ingredient.getTitle().equals(ingredientDto.name())
-                    )
-            ).toList();
-
-        if (!missingIngredients.isEmpty()) {
-            List<Ingredient> createdIngredients = ingredientService.createAll(missingIngredients);
-            ingredientList.addAll(createdIngredients);
-        }
-        return ingredientList;
     }
 }
