6673f7332d61e99a8444b1909b71e73cfa6f77e3
Ivan Andreev <e12025981@student.tuwien.ac.at>
2023-12-11 10:47:21 +0100

    [KV] add comments to CookingService

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/CookingService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/CookingService.java
@@ -12,11 +12,11 @@ import java.util.Optional;
 
 public interface CookingService {
 
-
     /**
-     * Get a list of recipe suggestions based on the provided store ID.
+     * Get a list of recipe suggestions based on the provided store ID and type.
      *
      * @param storId The ID of the storage.
+     * @param type   The type of the recipe (e.g., breakfast, main dish ...).
      * @return A list of recipe suggestions.
      * @throws ValidationException If there is a validation error.
      */
@@ -31,20 +31,52 @@ public interface CookingService {
     RecipeDetailDto getRecipeDetails(Long recipeId);
 
     /**
-     * Get a list of recipe suggestions from the cookbook.
+     * Get a list of recipes from the cookbook.
      *
-     * @return A list of recipe suggestions.
+     * @return A list of recipes.
      * @throws ValidationException If there is a validation error.
      */
     List<RecipeSuggestionDto> getCookbook() throws ValidationException;
 
+    /**
+     * Create a new recipe in the cookbook.
+     *
+     * @param recipe The recipe details to be added to the cookbook.
+     * @return The created recipe.
+     * @throws ConflictException If there is a conflict with existing data.
+     */
     RecipeSuggestion createCookbookRecipe(RecipeSuggestionDto recipe) throws ConflictException;
 
+    /**
+     * Get a specific recipe from the cookbook based on its ID.
+     *
+     * @param id The ID of the recipe to retrieve.
+     * @return An Optional containing the recipe, if found.
+     */
     Optional<RecipeSuggestion> getCookbookRecipe(Long id);
 
+    /**
+     * Update an existing recipe in the cookbook.
+     *
+     * @param recipe The updated recipe details.
+     * @return The updated recipe.
+     * @throws ConflictException If there is a conflict with existing data.
+     */
     RecipeSuggestion updateCookbookRecipe(RecipeSuggestionDto recipe) throws ConflictException;
 
+    /**
+     * Delete a recipe from the cookbook based on its ID.
+     *
+     * @param id The ID of the recipe to be deleted.
+     * @return The deleted recipe.
+     */
     RecipeSuggestion deleteCookbookRecipe(Long id);
 
+    /**
+     * Get a list of missing ingredients for a specific recipe from the cookbook.
+     *
+     * @param id The ID of the recipe to check for missing ingredients.
+     * @return The missing ingredients for the recipe.
+     */
     RecipeSuggestionDto getMissingIngredients(Long id);
 }
