d6094e7f1e1926892ede191b2fa496854e07ff01
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-12 06:25:59 +0100

    [FV] feat: adds create endpoint
    Refs: #208

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ExpenseEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ExpenseEndpoint.java
@@ -2,11 +2,14 @@ package at.ac.tuwien.sepr.groupphase.backend.endpoint;
 
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ExpenseDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.ExpenseMapper;
+import at.ac.tuwien.sepr.groupphase.backend.exception.ConflictException;
+import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
 import at.ac.tuwien.sepr.groupphase.backend.service.ExpenseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +36,15 @@ public class ExpenseEndpoint {
 
         return expenseMapper.entityToExpenseDto(
             expenseService.findById(id, jwt)
+        );
+    }
+
+    @PostMapping
+    public ExpenseDto create(ExpenseDto expenseDto, @RequestHeader("Authorization") String jwt) throws ValidationException, ConflictException {
+        LOGGER.info("create: {}", expenseDto);
+
+        return expenseMapper.entityToExpenseDto(
+            expenseService.create(expenseDto, jwt)
         );
     }
 
