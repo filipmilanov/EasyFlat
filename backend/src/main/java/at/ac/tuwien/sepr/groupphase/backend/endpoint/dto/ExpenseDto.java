ca6caec94773dcef2643136dbc1e47742e094451
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-12 08:47:09 +0100

    [FV] feat: validation for expense
    Refs: #196

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/ExpenseDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/ExpenseDto.java
@@ -1,18 +1,22 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
 import io.soabase.recordbuilder.core.RecordBuilder;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 @RecordBuilder
 public record ExpenseDto(
     Long id,
-    String title,
-    String description,
+    @NotEmpty(message = "Title cannot be empty") String title,
+    @NotNull(message = "Description cannot be null") String description,
+    @NotNull(message = "Amount cannot be empty")
     Long amountInCents,
+    @NotNull(message = "Payer cannot be null")
     UserDetailDto paidBy,
     List<DebitDto> debitUsers,
-    WgDetailDto sharedFlat
+    @NotNull(message = "The shared flat cannot be empty") WgDetailDto sharedFlat
 ) {
 
 }
