c576a2c160ff2a46fa1c376345e2d1d8774eff27
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-12 06:48:33 +0100

    [FV] feat: adds mapper from dto to entity
    Refs: #208

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/ExpenseMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/ExpenseMapper.java
@@ -1,13 +1,30 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper;
 
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.DebitDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.DebitDtoBuilder;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ExpenseDto;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Expense;
+import at.ac.tuwien.sepr.groupphase.backend.entity.SplitBy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(uses = {UserMapper.class, SharedFlatMapper.class, DebitMapper.class})
 public abstract class ExpenseMapper {
 
     @Mapping(target = "debitUsers", source = "expense")
     public abstract ExpenseDto entityToExpenseDto(Expense expense);
+
+    @Mapping(target = "splitBy", source = "debitUsers")
+    @Mapping(target = "debitUsers", source = "expenseDto")
+    public abstract Expense expenseDtoToExpense(ExpenseDto expenseDto);
+
+    public SplitBy splitByFromDebitDtoList(List<DebitDto> debitUsersDtoList) {
+        return debitUsersDtoList.stream()
+            .findAny()
+            .orElse(DebitDtoBuilder.builder().splitBy(null).build())
+            .splitBy();
+    }
+
 }
