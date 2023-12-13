82222c789332314ffe4d4188af4b3228b8115a53
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-13 23:22:36 +0100

    feat[FV-196]: adds DTOs for finance
    Refs: #196

---
+++ frontend/src/app/dtos/expenseDto.ts
@@ -0,0 +1,26 @@
+import {SharedFlat} from "./sharedFlat";
+import {UserDetail} from "./auth-request";
+
+export class ExpenseDto {
+  id?: number;
+  title?: string;
+  description?: string;
+  amountInCents?: number;
+  paidBy?: UserDetail;
+  debitUsers?: DebitDto[];
+  sharedFlat?: SharedFlat;
+}
+
+export class DebitDto {
+  user?: UserDetail;
+  splitBy?: SplitBy;
+  value?: number;
+}
+
+export enum SplitBy {
+  EQUAL = "EQUAL",
+  UNEQUAL = "UNEQUAL",
+  PERCENTAGE = "PERCENTAGE",
+  PROPORTIONAL = "PROPORTIONAL",
+}
+
