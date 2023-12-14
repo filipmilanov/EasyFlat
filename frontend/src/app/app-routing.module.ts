bc32315909408a3ce876c6f21f902094c3c00a61
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-14 16:26:44 +0100

    feat[FV]: define path for creating expenses
    Refs: #196

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,38 +4,50 @@ import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
 import {DigitalStorageComponent} from "./components/digital-storage/digital-storage.component";
 import {
-    ItemCreateEditComponent,
-    ItemCreateEditMode
+  ItemCreateEditComponent,
+  ItemCreateEditMode
 } from "./components/digital-storage/item-create-edit/item-create-edit.component";
 import {ItemDetailComponent} from "./components/digital-storage/item-detail/item-detail.component";
 import {ItemDetailListComponent} from "./components/digital-storage/item-detail-list/item-detail-list.component";
-import {StorageItemListDto} from "./dtos/storageItem";
 import {AuthGuard} from './guards/auth.guard';
 import {MessageComponent} from './components/message/message.component';
 import {RegisterComponent} from "./components/register/register.component";
 import {AccountComponent} from "./components/account/account.component";
 import {LoginFlatComponent} from "./components/login-flat/login-flat.component";
 import {CreateFlatComponent} from "./components/create-flat/create-flat.component";
+import {ExpenseCreateEditComponent} from "./components/finance/expense-create-edit/expense-create-edit.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'digital-storage/:id', children: [
+  {
+    path: 'digital-storage/:id', children: [
       {path: '', component: DigitalStorageComponent},
-      {path: 'item', children: [
+      {
+        path: 'item', children: [
           {path: 'create', component: ItemCreateEditComponent, data: {mode: ItemCreateEditMode.create}},
-          {path: ':name/list', children:[
-            {path: '', component: ItemDetailListComponent },
-            {path: ':id/detail', component: ItemDetailComponent},
+          {
+            path: ':name/list', children: [
+              {path: '', component: ItemDetailListComponent},
+              {path: ':id/detail', component: ItemDetailComponent},
               {path: ':id/edit', component: ItemCreateEditComponent, data: {mode: ItemCreateEditMode.edit}},
-            ]}
-        ]}
-  ]},
+            ]
+          }
+        ]
+      }
+    ]
+  },
   {path: 'message', canActivate: mapToCanActivate([AuthGuard]), component: MessageComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'account', component: AccountComponent},
   {path: 'wgLogin', component: LoginFlatComponent},
   {path: 'wgCreate', component: CreateFlatComponent},
+  {
+    path: 'finance', children: [
+      {path: '', component: DigitalStorageComponent},
+      {path: 'expense/create', component: ExpenseCreateEditComponent, data: {mode: ItemCreateEditMode.create}},
+    ]
+  }
 ];
 
 @NgModule({
