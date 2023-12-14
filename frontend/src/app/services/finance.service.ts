3f981edc73a2acc468beb2d3d0145cd64c134973
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-14 17:59:47 +0100

    feat[FV]: create finance service
    Refs: #196

---
+++ frontend/src/app/services/finance.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {ExpenseDto} from "../dtos/expenseDto";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FinanceService {
+  baseUri = environment.backendUrl + '/expense';
+
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
+
+  createExpense(expense: ExpenseDto): Observable<ExpenseDto> {
+    return this.http.post<ExpenseDto>(this.baseUri, expense);
+  }
+}
