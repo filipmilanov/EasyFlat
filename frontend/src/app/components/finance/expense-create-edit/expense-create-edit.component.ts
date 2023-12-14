083d78d4ecd7da22ac8df5963461e2eac68bb523
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-14 18:30:43 +0100

    feat[FV]: implements select strategy
    Refs: #196

--- frontend/src/app/components/finance/expense-create-edit/expense-create-edit.component.ts
+++ frontend/src/app/components/finance/expense-create-edit/expense-create-edit.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {DebitDto, ExpenseDto, SplitBy} from "../../../dtos/expenseDto";
 import {NgForm} from "@angular/forms";
+import {SharedFlat} from "../../../dtos/sharedFlat";
+import {SharedFlatService} from "../../../services/sharedFlat.service";
+import {FinanceService} from "../../../services/finance.service";
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-expense-create-edit',
@@ -12,10 +17,10 @@ export class ExpenseCreateEditComponent implements OnInit {
   heading: string = 'Create Expense';
   expense: ExpenseDto = new ExpenseDto();
   splitByOptions = Object.keys(SplitBy).map(key => ({value: key, label: SplitBy[key]}));
+  selectedSplitBy: SplitBy = SplitBy.EQUAL;
   submitButtonText: string = 'Create';
   users: DebitDto[] = [
     {
-      isTarget: true,
       user: {
         firstName: 'Max',
         lastName: 'Mustermann',
@@ -26,7 +31,6 @@ export class ExpenseCreateEditComponent implements OnInit {
       }
     },
     {
-      isTarget: true,
       user: {
         firstName: 'Max',
         lastName: 'Mustermann',
@@ -38,11 +42,40 @@ export class ExpenseCreateEditComponent implements OnInit {
     }
   ];
 
+  constructor(
+    private sharedFlatService: SharedFlatService,
+    private financeService: FinanceService,
+    private router: Router,
+    private notification: ToastrService,
+  ) {
+  }
+
   ngOnInit(): void {
 
   }
 
   onSubmit(form: NgForm) {
+    let o = this.financeService.createExpense(this.expense).subscribe({
+      next: (expense: ExpenseDto) => {
+        this.notification.success("Expense created");
+        this.router.navigate(['/finance']);
+      },
+      error: (error) => {
+        console.log(error);
+        let firstBracket = error.error.indexOf('[');
+        let lastBracket = error.error.indexOf(']');
+        let errorMessages = error.error.substring(firstBracket + 1, lastBracket).split(',');
+        let errorDescription = error.error.substring(0, firstBracket);
+        errorMessages.forEach(message => {
+          this.notification.error(message, errorDescription);
+        });
+      }
+    });
+  }
 
+  onSplitByChange() {
+    this.users.forEach(user => {
+      user.splitBy = this.selectedSplitBy;
+    });
   }
 }
