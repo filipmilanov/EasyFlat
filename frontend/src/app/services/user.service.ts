01884bf37029f7b002e739b76989605e3e68cf86
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-14 19:15:40 +0100

    feat[FV]: loading flatmates on load of finance create
    Refs: #196

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {environment} from "../../environments/environment";
 import {UserListDto} from "../dtos/user";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
