d76aa940cc689f4d2ef3e96338317a30b59cd620
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-14 16:26:18 +0100

    feat[FV]: create expense view
    Refs: #196

--- frontend/src/app/components/finance/expense-create-edit/expense-create-edit.component.ts
+++ frontend/src/app/components/finance/expense-create-edit/expense-create-edit.component.ts
@@ -1,10 +1,47 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DebitDto, ExpenseDto, SplitBy} from "../../../dtos/expenseDto";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-expense-create-edit',
   templateUrl: './expense-create-edit.component.html',
   styleUrls: ['./expense-create-edit.component.scss']
 })
-export class ExpenseCreateEditComponent {
+export class ExpenseCreateEditComponent implements OnInit {
 
+  heading: string = 'Create Expense';
+  expense: ExpenseDto = new ExpenseDto();
+  splitByOptions = Object.keys(SplitBy).map(key => ({value: key, label: SplitBy[key]}));
+  submitButtonText: string = 'Create';
+  users: DebitDto[] = [
+    {
+      isTarget: true,
+      user: {
+        firstName: 'Max',
+        lastName: 'Mustermann',
+        email: 'a@a.c',
+        flatName: 'WG',
+        password: '123',
+        admin: true
+      }
+    },
+    {
+      isTarget: true,
+      user: {
+        firstName: 'Max',
+        lastName: 'Mustermann',
+        email: 'a@a.c',
+        flatName: 'WG',
+        password: '123',
+        admin: true
+      }
+    }
+  ];
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+
+  }
 }
