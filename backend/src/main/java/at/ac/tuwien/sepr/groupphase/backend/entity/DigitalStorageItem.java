5ce0ca345a9a98f140d1d6be1a3e34ed36254e69
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-18 19:09:14 +0100

    feat: moves fields to ItemCache
    Refs: #221

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/DigitalStorageItem.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/DigitalStorageItem.java
@@ -13,10 +13,7 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.GroupSequence;
-import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
@@ -34,41 +31,20 @@ public class DigitalStorageItem {
     private Long itemId;
 
     @Column
-    private String ean;
-
-    @Column
-    private String generalName;
-
-    @Column
-    @NotEmpty(message = "The product name cannot be empty")
-    private String productName;
-
-    @Column
-    private String brand;
-
-    @Column
-    @Min(value = 0, message = "The actual quantity must be positive")
     private Double quantityCurrent;
-
-    @Column
-    @Min(value = 0, message = "The total quantity must be positive")
-    private Double quantityTotal;
-
-    @ManyToOne
-    private Unit unit;
 
     @Column
     @FutureOrPresent(message = "You cannot store products which are over the expire date")
     private LocalDate expireDate;
 
     @Column
-    private String description;
-
-    @Column
     private Long priceInCent;
 
     @Column
     private String boughtAt;
+
+    @ManyToOne
+    private ItemCache itemCache;
 
     @ManyToOne
     @NotNull(message = "A DigitalStorageItem need to be linked to a storage")
@@ -80,49 +56,12 @@ public class DigitalStorageItem {
     @OneToMany
     private List<ItemStats> itemStats;
 
-    @AssertTrue(message = "The current quantity cannot be larger then the total")
-    private boolean quantityCurrentLessThenTotal() {
-        return this.quantityCurrent <= this.quantityTotal;
-    }
-
     public Long getItemId() {
         return itemId;
     }
 
     public void setItemId(Long id) {
         this.itemId = id;
-    }
-
-    public String getEan() {
-        return ean;
-    }
-
-    public void setEan(String ean) {
-        this.ean = ean;
-    }
-
-    public String getGeneralName() {
-        return generalName;
-    }
-
-    public void setGeneralName(String generalName) {
-        this.generalName = generalName;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
     }
 
     public Double getQuantityCurrent() {
@@ -133,36 +72,12 @@ public class DigitalStorageItem {
         this.quantityCurrent = quantityCurrent;
     }
 
-    public Double getQuantityTotal() {
-        return quantityTotal;
-    }
-
-    public void setQuantityTotal(Double quantityTotal) {
-        this.quantityTotal = quantityTotal;
-    }
-
-    public Unit getUnit() {
-        return unit;
-    }
-
-    public void setUnit(Unit unit) {
-        this.unit = unit;
-    }
-
     public LocalDate getExpireDate() {
         return expireDate;
     }
 
     public void setExpireDate(LocalDate expireDate) {
         this.expireDate = expireDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public Long getPriceInCent() {
@@ -179,6 +94,22 @@ public class DigitalStorageItem {
 
     public void setBoughtAt(String boughtAt) {
         this.boughtAt = boughtAt;
+    }
+
+    public ItemCache getItemCache() {
+        return itemCache;
+    }
+
+    public void setItemCache(ItemCache itemCache) {
+        this.itemCache = itemCache;
+    }
+
+    public DigitalStorage getDigitalStorage() {
+        return digitalStorage;
+    }
+
+    public void setDigitalStorage(DigitalStorage digitalStorage) {
+        this.digitalStorage = digitalStorage;
     }
 
     public DigitalStorage getStorage() {
