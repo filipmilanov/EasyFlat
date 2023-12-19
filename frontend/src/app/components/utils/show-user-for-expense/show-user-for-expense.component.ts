21ea5fd5462f8b27388099804b6306852b687b7b
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-19 10:06:55 +0100

    feat: default values corresponding to the selected users and split strategy
    Refs: 196

--- frontend/src/app/components/utils/show-user-for-expense/show-user-for-expense.component.ts
+++ frontend/src/app/components/utils/show-user-for-expense/show-user-for-expense.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
 import {DebitDto, SplitBy} from "../../../dtos/expenseDto";
 
 @Component({
@@ -6,25 +6,69 @@ import {DebitDto, SplitBy} from "../../../dtos/expenseDto";
   templateUrl: './show-user-for-expense.component.html',
   styleUrls: ['./show-user-for-expense.component.scss']
 })
-export class ShowUserForExpenseComponent {
+export class ShowUserForExpenseComponent implements OnChanges {
+  @Input() amountInEuro: number;
+  @Input() splitBy: SplitBy;
   @Input() users: DebitDto[];
 
   @Output() usersChange = new EventEmitter<DebitDto[]>();
+
+  selectedUsers: boolean[] = [];
 
   onUsersChange() {
     this.usersChange.emit(this.users);
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.updateSelectedUsersArray();
+    this.adaptedToChange();
+  }
+
   kindOfValue(value: DebitDto): string {
-    if (value.splitBy === SplitBy.EQUAL) {
+    if (this.splitBy === SplitBy.EQUAL) {
       return '€';
-    } else if (value.splitBy === SplitBy.UNEQUAL) {
+    } else if (this.splitBy === SplitBy.UNEQUAL) {
       return '€';
-    } else if (value.splitBy === SplitBy.PERCENTAGE) {
+    } else if (this.splitBy === SplitBy.PERCENTAGE) {
       return '%';
-    } else if (value.splitBy === SplitBy.PROPORTIONAL) {
+    } else if (this.splitBy === SplitBy.PROPORTIONAL) {
       return 'Proportion';
     }
+  }
+
+  adaptedToChange() {
+    if (this.splitBy === SplitBy.EQUAL) {
+      this.users.forEach(user => {
+        if (this.selectedUsers[this.users.indexOf(user)]) {
+          user.value = (this.amountInEuro ? this.amountInEuro : 0) / this.sizeOfSelectedUsers();
+        } else {
+          user.value = 0;
+        }
+      })
+    } else if (this.splitBy === SplitBy.PERCENTAGE) {
+      this.users.forEach(user => {
+        if (this.selectedUsers[this.users.indexOf(user)]) {
+          user.value = 100 / this.sizeOfSelectedUsers();
+        } else {
+          user.value = 0;
+        }
+      })
+    } else {
+      this.users.forEach(user => {
+        user.value = 0
+      })
+    }
+  }
+
+
+  private sizeOfSelectedUsers(): number {
+    return this.selectedUsers.filter(value => value).length;
+  }
+
+  private updateSelectedUsersArray() {
+    this.users.forEach((value, index) => {
+      this.selectedUsers[index] = this.selectedUsers[index] !== false;
+    });
   }
 
 }
