791eb1b097fad297fc4f0cd89e965d940dd490be
Philipp Schoeller <e11907084@student.tuwien.ac.at>
2023-12-21 19:09:13 +0000

    fix: roll permission

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ExpenseEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ExpenseEndpoint.java
@@ -35,7 +35,7 @@ public class ExpenseEndpoint {
         this.expenseMapper = expenseMapper;
     }
 
-    @PermitAll
+    @Secured("ROLE_USER")
     @GetMapping("{id}")
     public ExpenseDto findById(@PathVariable("id") Long id, @RequestHeader("Authorization") String jwt) {
         LOGGER.info("findById: {}", id);
