9bd2736e82fae75e88c2f78695092518e6516b43
Filip Milanov <e12023969@student.tuwien.ac.at>
2023-12-27 22:39:51 +0100

    [HP-8.8] Lists of chores

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/ChoreMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/ChoreMapper.java
@@ -5,6 +5,8 @@ import at.ac.tuwien.sepr.groupphase.backend.entity.Chore;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper
 public abstract class ChoreMapper {
 
@@ -13,4 +15,7 @@ public abstract class ChoreMapper {
 
     @Mapping(target = "choreName", source = "name")
     public abstract ChoreDto entityToChoreDto(Chore chore);
+
+    @Mapping(target = "choreName", source = "name")
+    public abstract List<ChoreDto> entityListToDtoList(List<Chore> lists);
 }
