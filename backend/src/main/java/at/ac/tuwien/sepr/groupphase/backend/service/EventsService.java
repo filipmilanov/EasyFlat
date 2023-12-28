47d515365032fd168d0973c5396b8cc51d445b9f
Ivan Andreev <e12025981@student.tuwien.ac.at>
2023-12-28 12:56:35 +0200

    [GE] Add JavaDoc

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/EventsService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/EventsService.java
@@ -6,15 +6,53 @@ import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
 
 import java.util.List;
 
+/**
+ * Service interface for managing events.
+ */
 public interface EventsService {
 
+    /**
+     * Creates a new event.
+     *
+     * @param event The EventDto object representing the new event.
+     * @return The created EventDto.
+     * @throws ValidationException If the event validation fails.
+     */
     EventDto create(EventDto event) throws ValidationException;
 
+    /**
+     * Updates an existing event.
+     *
+     * @param event The EventDto object representing the updated event.
+     * @return The updated EventDto.
+     * @throws AuthorizationException If the user is not authorized to update the event.
+     * @throws ValidationException If the event validation fails.
+     */
     EventDto update(EventDto event) throws AuthorizationException, ValidationException;
 
+    /**
+     * Deletes an event by its ID.
+     *
+     * @param id The ID of the event to be deleted.
+     * @return The deleted EventDto.
+     * @throws AuthorizationException If the user is not authorized to delete the event.
+     */
     EventDto delete(Long id) throws AuthorizationException;
 
+    /**
+     * Retrieves a list of all events.
+     *
+     * @return A list of EventDto objects representing all events.
+     */
     List<EventDto> findAll();
 
+    /**
+     * Retrieves an event by its ID.
+     *
+     * @param id The ID of the event to be retrieved.
+     * @return The EventDto with the specified ID.
+     * @throws AuthorizationException If the user is not authorized to access the event.
+     */
     EventDto getEventWithId(Long id) throws AuthorizationException;
 }
+
