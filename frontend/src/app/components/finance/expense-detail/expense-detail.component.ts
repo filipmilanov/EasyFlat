77fe6a04d1844aa9cc9dac98a96aa999a7976bef
Alex Ofner <e12119038@student.tuwien.ac.at>
2023-12-29 11:28:31 +0100

    feat: impl loading data
    Refs: #202

--- frontend/src/app/components/finance/expense-detail/expense-detail.component.ts
+++ frontend/src/app/components/finance/expense-detail/expense-detail.component.ts
@@ -1,10 +1,60 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FinanceService} from "../../../services/finance.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
+import {ExpenseDto} from "../../../dtos/expenseDto";
 
 @Component({
   selector: 'app-expense-detail',
   templateUrl: './expense-detail.component.html',
   styleUrls: ['./expense-detail.component.scss']
 })
-export class ExpenseDetailComponent {
+export class ExpenseDetailComponent implements OnInit {
+
+
+  expense: ExpenseDto;
+
+  constructor(
+    private financeService: FinanceService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private notification: ToastrService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe({
+      next: params => {
+        this.financeService.findById(params.id).subscribe({
+          next: res => {
+            this.expense = res;
+          },
+          error: error => {
+            console.error("Error finding expense:", error);
+            this.router.navigate(['/expense']);
+            let firstBracket = error.error.indexOf('[');
+            let lastBracket = error.error.indexOf(']');
+            let errorMessages = error.error.substring(firstBracket + 1, lastBracket).split(',');
+            let errorDescription = error.error.substring(0, firstBracket - 2);
+            errorMessages.forEach(message => {
+              this.notification.error(message, errorDescription);
+            });
+          }
+        });
+      },
+      error: error => {
+        console.error("Error fetching parameters:", error);
+        this.router.navigate(['/expense']);
+        let firstBracket = error.error.indexOf('[');
+        let lastBracket = error.error.indexOf(']');
+        let errorMessages = error.error.substring(firstBracket + 1, lastBracket).split(',');
+        let errorDescription = error.error.substring(0, firstBracket - 2);
+        errorMessages.forEach(message => {
+          this.notification.error(message, errorDescription);
+        });
+      }
+    });
+  }
+
 
 }
