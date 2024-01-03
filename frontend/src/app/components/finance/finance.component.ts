46908b506d952bca5fe2451a9fca672fc55ff073
Alex Ofner <e12119038@student.tuwien.ac.at>
2024-01-03 17:50:57 +0100

    feat: embedded debit component in finance
    Refs: #201

--- frontend/src/app/components/finance/finance.component.ts
+++ frontend/src/app/components/finance/finance.component.ts
@@ -1,10 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BalanceDebitDto} from "../../dtos/expenseDto";
+import {ToastrService} from "ngx-toastr";
+import {FinanceService} from "../../services/finance.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-finance',
   templateUrl: './finance.component.html',
   styleUrls: ['./finance.component.scss']
 })
-export class FinanceComponent {
+export class FinanceComponent implements OnInit {
+
+  balanceDebits: BalanceDebitDto[] = [];
+
+  constructor(
+      private financeService: FinanceService,
+      private notification: ToastrService,
+      private activatedRoute: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit(): void {
+    let o = this.financeService.findBalanceDebits().subscribe({
+      next: (balanceDebits) => {
+        console.log(balanceDebits);
+        this.balanceDebits = balanceDebits;
+      },
+      error: (error) => {
+        console.log(error);
+        this.notification.error("Could not load balance debits", "Error");
+      }
+    })
+  }
+
+
 
 }
+
