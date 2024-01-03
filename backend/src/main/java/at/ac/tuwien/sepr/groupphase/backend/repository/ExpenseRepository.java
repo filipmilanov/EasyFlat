1cb717b15c43a16bf9b0c3464668296ca40eb768
Alex Ofner <e12119038@student.tuwien.ac.at>
2024-01-03 17:10:02 +0100

    feat: creates basic repository functionalities for expense calculation
    Refs: #201

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ExpenseRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ExpenseRepository.java
@@ -1,8 +1,16 @@
 package at.ac.tuwien.sepr.groupphase.backend.repository;
 
+import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Set;
+
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    List<Expense> findAllByPaidByIs(ApplicationUser user);
+
+    List<Expense> findByPaidByIsIn(Set<ApplicationUser> users);
 }
 
