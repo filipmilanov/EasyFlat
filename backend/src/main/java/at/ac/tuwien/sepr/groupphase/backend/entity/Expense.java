506bb2a6952c0d96e2d039cfbf230f902b6b24b1
Alex Ofner <e12119038@student.tuwien.ac.at>
2024-01-03 10:34:43 +0100

    fix: added getter and setter for createAt
    the mapper was not working correct
    
    Refs: #254

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/Expense.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/Expense.java
@@ -105,6 +105,14 @@ public class Expense {
         this.debitUsers = debitUsers;
     }
 
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
