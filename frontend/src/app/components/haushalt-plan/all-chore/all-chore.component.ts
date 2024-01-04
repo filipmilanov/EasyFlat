cb5a37c8037061be966a216f4b01089bc6c47c81
Marin Stanev <e12122383@student.tuwien.ac.at>
2024-01-04 21:52:49 +0200

    delete completed chores

--- frontend/src/app/components/haushalt-plan/all-chore/all-chore.component.ts
+++ frontend/src/app/components/haushalt-plan/all-chore/all-chore.component.ts
@@ -5,9 +5,6 @@ import {ChoresDto, ChoreSearchDto} from "../../../dtos/chores";
 import {ShoppingListService} from "../../../services/shopping-list.service";
 import {ToastrService} from "ngx-toastr";
 import {ChoreService} from "../../../services/chore.service";
-import {forEach} from "lodash";
-import {AuthService} from "../../../services/auth.service";
-import {resolve} from "@angular/compiler-cli";
 
 @Component({
   selector: 'app-all-chore',
@@ -16,7 +13,6 @@ import {resolve} from "@angular/compiler-cli";
 })
 export class AllChoreComponent {
   chores: ChoresDto[];
-  completedChores: ChoresDto[] = [];
   searchParams: ChoreSearchDto = {
     userName: '',
     endDate: null,
@@ -24,8 +20,7 @@ export class AllChoreComponent {
 
   constructor(private router: Router,
   private choreService: ChoreService,
-  private notification: ToastrService,
-              private authService: AuthService) { }
+  private notification: ToastrService) { }
 
   navigateToNewChore() {
     this.router.navigate(['/chores', 'add']);
@@ -33,7 +28,6 @@ export class AllChoreComponent {
 
   ngOnInit() {
     this.loadChores();
-    console.log(this.chores)
   }
 
   loadChores() {
@@ -52,59 +46,11 @@ export class AllChoreComponent {
     this.choreService.assginChores().subscribe({
       next: res => {
         this.chores = res;
-        this.router.navigate(['/chores/all']);
       },
       error: err => {
         console.error("Error fetching chores with users")
-        this.notification.error("Error assigning chores with users")
+        this.notification.error("Error loading chores")
       }
     });
-  }
-
-  updateChoreComplete(chore: ChoresDto) {
-    for (let i = 0; i < this.chores.length; i++) {
-      if (this.chores[i] == chore) {
-        this.chores[i].completed = !this.chores[i].completed;
-        break;
-      }
-    }
-    this.completedChores = this.chores.filter(chore => chore.completed);
-    console.log(this.completedChores)
-  }
-
-  completedChoresIsEmpty() {
-    return this.completedChores.length == 0;
-  }
-
-  deleteCompletedChores() {
-    return this.choreService.deleteChores(this.completedChores).subscribe({
-      next: res => {
-        this.notification.success(`Chores completed.`, "Success");
-        for (let i = 0; i < res.length; i++) {
-          this.chores = this.chores.filter(chore => chore.id !== res[i].id);
-        }
-        this.awardPoints();
-        this.completedChores = [];
-
-      },
-      error: err => {
-        console.error("Chores could not be deleted");
-      }
-    });
-  }
-
-  awardPoints() {
-    for (let i = 0; i < this.completedChores.length; i++) {
-      let curr = this.completedChores[i];
-      let points = curr.points + 20;
-      this.authService.updatePoints(points, curr.user.id).subscribe({
-        next: res => {
-          this.notification.success("Points awarded.", "Success");
-        },
-        error: err => {
-          console.error("Application users could not be update");
-        }
-      });
-    }
   }
 }
