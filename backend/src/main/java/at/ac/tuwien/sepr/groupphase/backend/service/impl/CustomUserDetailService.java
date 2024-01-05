f984d8ffab70323675598fe73f90bc4881d34fe1
Marin Stanev <e12122383@student.tuwien.ac.at>
2024-01-05 09:17:24 +0200

    show leaderboard based on users' points

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/CustomUserDetailService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/CustomUserDetailService.java
@@ -28,7 +28,6 @@ import org.springframework.stereotype.Service;
 
 import java.lang.invoke.MethodHandles;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class CustomUserDetailService implements UserService {
