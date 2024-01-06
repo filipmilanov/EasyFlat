16d8c1cc572e9277dec23502cd7071a7c575532c
Marin Stanev <e12122383@student.tuwien.ac.at>
2024-01-06 11:23:32 +0200

    complete my chores

--- frontend/src/app/components/haushalt-plan/my-chores/my-chores.component.ts
+++ frontend/src/app/components/haushalt-plan/my-chores/my-chores.component.ts
@@ -3,6 +3,7 @@ import {Router} from "@angular/router";
 import {ChoreService} from "../../../services/chore.service";
 import {ToastrService} from "ngx-toastr";
 import {ChoresDto} from "../../../dtos/chores";
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
   selector: 'app-my-chores',
@@ -11,11 +12,13 @@ import {ChoresDto} from "../../../dtos/chores";
 })
 export class MyChoresComponent {
   chores: ChoresDto[];
+  completedChores: ChoresDto[] = [];
   private searchParams: string;
 
   constructor(private router: Router,
               private choreService: ChoreService,
-              private notification: ToastrService) { }
+              private notification: ToastrService,
+              private authService: AuthService) { }
 
   ngOnInit() {
     this.choreService.getChoresByUser(this.searchParams).subscribe({
@@ -30,6 +33,53 @@ export class MyChoresComponent {
         this.notification.error("Error loading chores")
       }
     });
+  }
+
+  updateChoreComplete(chore: ChoresDto) {
+    for (let i = 0; i < this.chores.length; i++) {
+      if (this.chores[i] == chore) {
+        this.chores[i].completed = !this.chores[i].completed;
+        break;
+      }
+    }
+    this.completedChores = this.chores.filter(chore => chore.completed);
+    console.log(this.completedChores)
+  }
+
+  completedChoresIsEmpty() {
+    return this.completedChores.length == 0;
+  }
+
+  deleteCompletedChores() {
+    return this.choreService.deleteChores(this.completedChores).subscribe({
+      next: res => {
+        this.notification.success(`Chores completed.`, "Success");
+        for (let i = 0; i < res.length; i++) {
+          this.chores = this.chores.filter(chore => chore.id !== res[i].id);
+        }
+        this.awardPoints();
+        this.completedChores = [];
+
+      },
+      error: err => {
+        console.error("Chores could not be deleted");
+      }
+    });
+  }
+
+  awardPoints() {
+    for (let i = 0; i < this.completedChores.length; i++) {
+      let curr = this.completedChores[i];
+      let points = curr.points + 20;
+      this.authService.updatePoints(points, curr.user.id).subscribe({
+        next: res => {
+          this.notification.success("Points awarded.", "Success");
+        },
+        error: err => {
+          console.error("Application users could not be update");
+        }
+      });
+    }
   }
 
 }
