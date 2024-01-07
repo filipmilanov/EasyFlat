26cb3cc7fc360829772ba4e2f26f712e1ec3a472
Marin Stanev <e12122383@student.tuwien.ac.at>
2024-01-07 12:44:31 +0200

    import exception

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/UserService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/UserService.java
@@ -3,6 +3,7 @@ package at.ac.tuwien.sepr.groupphase.backend.service;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserDetailDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserLoginDto;
 import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
+import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
