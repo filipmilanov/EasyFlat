2ba45f0d6cf101d451ad2d499330cf1c0b71aad3
Ivan Andreev <e12025981@student.tuwien.ac.at>
2024-01-07 09:55:51 +0100

    [KV-Matching] get matched Ingredients, for details, frontend.

--- frontend/src/app/components/cooking/recipe-detail/recipe-detail.component.ts
+++ frontend/src/app/components/cooking/recipe-detail/recipe-detail.component.ts
@@ -18,6 +18,7 @@ export class RecipeDetailComponent implements OnInit {
   recipeDetail: RecipeDetailDto;
   @Output() matchingClicked: EventEmitter<string> = new EventEmitter<string>();
 
+
   constructor(
     private service: CookingService,
     private notification: ToastrService,
@@ -47,42 +48,14 @@ export class RecipeDetailComponent implements OnInit {
   }
 
 
-
   openMatchModal(ingredient: RecipeIngredient) {
     const modalRef = this.modalService.open(MatchingModalComponent, {size: 'lg'});
     modalRef.componentInstance.ingredient = ingredient;
+
+    modalRef.componentInstance.matchingDone.subscribe(() => {
+      this.load();
+    });
   }
 
 
-
-  addTestItems() {
-
-    const testRecipe: RecipeDetailDto = {
-      id: 'test-id',
-      title: 'Test Recipe',
-      summary: 'This is a <b>test recipe</b> summary.',
-      servings: 4,
-      readyInMinutes: 30,
-      extendedIngredients: [
-        {id: 1, name: 'Ingredient 1', unit: 'g', amount: 100},
-        {id: 2, name: 'Ingredient 2', unit: 'ml', amount: 200},
-        {id: 3, name: 'Ingredient 3', unit: 'pieces', amount: 3},
-        {id: 4, name: 'Ingredient 4', unit: 'tsp', amount: 2},
-        // Add more ingredients as needed
-      ],
-      steps: {
-        steps: [
-          {number: 1, step: 'Step 1: Do something'},
-          {number: 2, step: 'Step 2: Do something else'},
-          {number: 3, step: 'Step 3: Do another thing'},
-          {number: 4, step: 'Step 4: Final step'},
-          // Add more steps as needed
-        ]
-      },
-      missedIngredients: [
-        {id: 5, name: 'Ingredient 5', unit: 'tsp', amount: 2},
-      ]
-    };
-    this.recipeDetail = testRecipe;
-  }
 }
