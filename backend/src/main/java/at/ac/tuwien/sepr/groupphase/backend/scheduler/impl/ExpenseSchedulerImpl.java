820faec58f382235e4e335852ab5babeaf63e33f
Alex Ofner <e12119038@student.tuwien.ac.at>
2024-01-08 11:28:39 +0100

    feat: creates scheduler interface

---
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/scheduler/impl/ExpenseSchedulerImpl.java
@@ -0,0 +1,15 @@
+package at.ac.tuwien.sepr.groupphase.backend.scheduler.impl;
+
+import at.ac.tuwien.sepr.groupphase.backend.scheduler.ExpenseScheduler;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExpenseSchedulerImpl implements ExpenseScheduler {
+
+    @Override
+    @Scheduled(cron = "0 0 0 * *")
+    public void createRepeatingExpense() {
+
+    }
+}
