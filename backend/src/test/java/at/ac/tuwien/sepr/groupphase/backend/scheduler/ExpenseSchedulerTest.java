dac98b0a7ba078541fb4d55b02ef3a5a6b2ebdd6
Alex Ofner <e12119038@student.tuwien.ac.at>
2024-01-08 15:19:35 +0100

    test: fix mocking for expenseService

--- backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/scheduler/ExpenseSchedulerTest.java
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/scheduler/ExpenseSchedulerTest.java
@@ -1,21 +1,30 @@
 package at.ac.tuwien.sepr.groupphase.backend.scheduler;
 
 import at.ac.tuwien.sepr.groupphase.backend.basetest.TestDataGenerator;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserListDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserListDtoBuilder;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.finance.DebitDtoBuilder;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.finance.ExpenseDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.finance.ExpenseDtoBuilder;
-import at.ac.tuwien.sepr.groupphase.backend.entity.Expense;
+import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
+import at.ac.tuwien.sepr.groupphase.backend.entity.SplitBy;
 import at.ac.tuwien.sepr.groupphase.backend.exception.AuthenticationException;
 import at.ac.tuwien.sepr.groupphase.backend.exception.ConflictException;
 import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
+import at.ac.tuwien.sepr.groupphase.backend.repository.UserRepository;
+import at.ac.tuwien.sepr.groupphase.backend.security.AuthService;
 import at.ac.tuwien.sepr.groupphase.backend.service.ExpenseService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
@@ -29,105 +38,56 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class ExpenseSchedulerTest {
 
     @Autowired
     private ExpenseScheduler expenseScheduler;
 
-    @MockBean
+    @SpyBean
     private ExpenseService expenseService;
 
     @Autowired
     private TestDataGenerator testDataGenerator;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @MockBean
+    private AuthService authService;
+    private ApplicationUser applicationUser;
+
     @BeforeEach
     public void cleanUp() throws ValidationException, ConflictException {
         testDataGenerator.cleanUp();
+
+        applicationUser = userRepository.findById(1L).orElseThrow();
+        when(authService.getUserFromToken()).thenReturn(applicationUser);
     }
 
     private static Stream<Arguments> createRepeatingExpensesData() {
         return Stream.of(
             Arguments.of(
-                List.of(ExpenseDtoBuilder.builder()
-                        .title("Repeating 1")
-                        .description("Repeating 1")
-                        .amountInCents(1000.0)
-                        .isRepeating(true)
-                        .periodInDays(1)
-                        .createdAt(LocalDateTime.now().minusDays(1))
-                        .build(),
-                    ExpenseDtoBuilder.builder()
-                        .title("Repeating 2")
-                        .description("Repeating 2")
-                        .amountInCents(1000.0)
-                        .isRepeating(true)
-                        .periodInDays(1)
-                        .createdAt(LocalDateTime.now().minusDays(1))
-                        .build(),
-                    ExpenseDtoBuilder.builder()
-                        .title("Repeating 2")
-                        .description("Repeating 2")
-                        .amountInCents(1000.0)
-                        .isRepeating(true)
-                        .periodInDays(1)
-                        .createdAt(LocalDateTime.now().minusDays(1))
-                        .build()
+                List.of(
+                    generateRepeatingExpense(1, LocalDateTime.now().minusDays(1)),
+                    generateRepeatingExpense(1, LocalDateTime.now().minusDays(1)),
+                    generateRepeatingExpense(1, LocalDateTime.now().minusDays(1))
                 ),
                 3
             ),
             Arguments.of(
-                List.of(ExpenseDtoBuilder.builder()
-                        .title("Repeating 1")
-                        .description("Repeating 1")
-                        .amountInCents(1000.0)
-                        .isRepeating(true)
-                        .periodInDays(1)
-                        .createdAt(LocalDateTime.now().minusDays(1))
-                        .build(),
-                    ExpenseDtoBuilder.builder()
-                        .title("Repeating 2")
-                        .description("Repeating 2")
-                        .amountInCents(1000.0)
-                        .isRepeating(true)
-                        .periodInDays(2)
-                        .createdAt(LocalDateTime.now().minusDays(1))
-                        .build(),
-                    ExpenseDtoBuilder.builder()
-                        .title("Repeating 2")
-                        .description("Repeating 2")
-                        .amountInCents(1000.0)
-                        .isRepeating(true)
-                        .periodInDays(3)
-                        .createdAt(LocalDateTime.now().minusDays(1))
-                        .build()
+                List.of(
+                    generateRepeatingExpense(1, LocalDateTime.now().minusDays(1)),
+                    generateRepeatingExpense(2, LocalDateTime.now().minusDays(1)),
+                    generateRepeatingExpense(3, LocalDateTime.now().minusDays(1))
                 ),
                 1
             ),
             Arguments.of(
-                List.of(ExpenseDtoBuilder.builder()
-                        .title("Repeating 1")
-                        .description("Repeating 1")
-                        .amountInCents(1000.0)
-                        .isRepeating(true)
-                        .periodInDays(2)
-                        .createdAt(LocalDateTime.now())
-                        .build(),
-                    ExpenseDtoBuilder.builder()
-                        .title("Repeating 2")
-                        .description("Repeating 2")
-                        .amountInCents(1000.0)
-                        .isRepeating(true)
-                        .periodInDays(1)
-                        .createdAt(LocalDateTime.now())
-                        .build(),
-                    ExpenseDtoBuilder.builder()
-                        .title("Repeating 2")
-                        .description("Repeating 2")
-                        .amountInCents(1000.0)
-                        .isRepeating(true)
-                        .periodInDays(2)
-                        .createdAt(LocalDateTime.now())
-                        .build()
+                List.of(
+                    generateRepeatingExpense(2, LocalDateTime.now()),
+                    generateRepeatingExpense(1, LocalDateTime.now()),
+                    generateRepeatingExpense(2, LocalDateTime.now())
                 ),
                 0
             ),
@@ -150,8 +110,6 @@ public class ExpenseSchedulerTest {
             }
         );
 
-        when(expenseService.create(any())).thenReturn(new Expense());
-
         // when
         expenseScheduler.createRepeatingExpense();
 
@@ -160,5 +118,37 @@ public class ExpenseSchedulerTest {
 
     }
 
+    private static ExpenseDto generateRepeatingExpense(Integer periodInDays, LocalDateTime createdAt) {
+
+        return ExpenseDtoBuilder.builder()
+            .title("Test")
+            .description("Test")
+            .amountInCents(50.0)
+            .createdAt(createdAt)
+            .paidBy(generateListUser(6))
+            .isRepeating(periodInDays != null)
+            .periodInDays(periodInDays)
+            .debitUsers(
+                List.of(
+                    DebitDtoBuilder.builder()
+                        .user(generateListUser(6))
+                        .splitBy(SplitBy.EQUAL)
+                        .value(25.0)
+                        .build(),
+                    DebitDtoBuilder.builder()
+                        .user(generateListUser(11))
+                        .splitBy(SplitBy.EQUAL)
+                        .value(25.0)
+                        .build()
+                )
+            )
+            .build();
+    }
+
+    private static UserListDto generateListUser(long id) {
+        return UserListDtoBuilder.builder()
+            .id(id)
+            .build();
+    }
 
 }
