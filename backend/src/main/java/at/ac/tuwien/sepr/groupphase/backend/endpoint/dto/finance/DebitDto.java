1afed5b98f62d4b516882e21ca544b9e0b1c3984
Alex Ofner <e12119038@student.tuwien.ac.at>
2024-01-11 11:22:39 +0100

    fix: adds validation for debitDto
    Refs: #268

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/finance/DebitDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/finance/DebitDto.java
@@ -3,14 +3,20 @@ package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.finance;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserListDto;
 import at.ac.tuwien.sepr.groupphase.backend.entity.SplitBy;
 import io.soabase.recordbuilder.core.RecordBuilder;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 /**
  * Data transfer object for debits which is used for all splitBy strategies.
  */
 @RecordBuilder
 public record DebitDto(
+    @NotNull(message = "The debit user must be defined")
     UserListDto user,
+
+    @NotNull(message = "The split strategy ist not defined")
     SplitBy splitBy,
+    @Positive(message = "The amount per user must be positive")
     Double value
 ) {
 }
