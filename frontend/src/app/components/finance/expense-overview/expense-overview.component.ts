e7d549abdd7e0521305fc0a3908086c711de0a68
Philipp Schoeller <e11907084@student.tuwien.ac.at>
2024-01-14 22:22:49 +0100

    [FV-4.5] feat: adjust expense-overview component for search
    Refs: #200

--- frontend/src/app/components/finance/expense-overview/expense-overview.component.ts
+++ frontend/src/app/components/finance/expense-overview/expense-overview.component.ts
@@ -1,8 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {ExpenseDto} from "../../../dtos/expenseDto";
+import {ExpenseDto, ExpenseSearchDto, RepeatingExpenseType} from "../../../dtos/expenseDto";
 import {FinanceService} from "../../../services/finance.service";
 import {ToastrService} from "ngx-toastr";
 import {Router} from "@angular/router";
+import {debounceTime, Subject} from "rxjs";
+import {UserListDto} from "../../../dtos/user";
+import {UserService} from "../../../services/user.service";
 
 @Component({
   selector: 'app-expense-overview',
@@ -12,30 +15,61 @@ import {Router} from "@angular/router";
 export class ExpenseOverviewComponent implements OnInit {
 
   expenses: ExpenseDto[];
+  searchParams: ExpenseSearchDto = {};
+  searchDate: string | null = null;
+  searchChangedObservable = new Subject<void>();
+  users: UserListDto[] = [];
 
-  constructor(private financeService: FinanceService,
+  constructor(private userService: UserService,
+              private financeService: FinanceService,
               private router: Router,
               private notification: ToastrService) {
   }
 
+  searchChanged(): void {
+    this.searchChangedObservable.next();
+  }
+
   ngOnInit(): void {
-    this.financeService.findAll().subscribe({
-      next: res => {
-        this.expenses = res;
+    this.userService.findFlatmates().subscribe({
+      next: (users) => {
+        this.users = users;
       },
-      error: error => {
-        console.error(`Expenses could not be loaded: ${error}`);
-        this.router.navigate(['/finance/']);
-        this.notification.error(`Expenses could not be loaded`, "Error");
-        let firstBracket = error.error.indexOf('[');
-        let lastBracket = error.error.indexOf(']');
-        let errorMessages = error.error.substring(firstBracket + 1, lastBracket).split(',');
-        let errorDescription = error.error.substring(0, firstBracket);
-        errorMessages.forEach(message => {
-          this.notification.error(message, errorDescription);
-        });
+      error: (error) => {
+        console.error(error);
+        this.notification.error("Could not load flatmates", "Error");
       }
-    })
+    });
+    this.reloadExpenses();
+    this.searchChangedObservable
+      .pipe(debounceTime(300))
+      .subscribe({next: () => this.reloadExpenses()});
+  }
+
+  reloadExpenses() {
+    if (this.searchDate == null || this.searchDate === "") {
+      delete this.searchParams.createdAt;
+    } else {
+      this.searchParams.createdAt = new Date(this.searchDate);
+    }
+    this.financeService.findAll(this.searchParams)
+      .subscribe({
+        next: res => {
+          this.expenses = res;
+        },
+        error: error => {
+          console.error(`Expenses could not be loaded: ${error}`);
+          this.router.navigate(['/finance/']);
+          this.notification.error(`Expenses could not be loaded`, "Error");
+          let firstBracket = error.error.indexOf('[');
+          let lastBracket = error.error.indexOf(']');
+          let errorMessages = error.error.substring(firstBracket + 1, lastBracket).split(',');
+          let errorDescription = error.error.substring(0, firstBracket);
+          errorMessages.forEach(message => {
+            this.notification.error(message, errorDescription);
+          });
+        }
+      });
   }
 
   public delete(expense: ExpenseDto): void {
@@ -57,4 +91,5 @@ export class ExpenseOverviewComponent implements OnInit {
     });
   }
 
+  protected readonly UserListDto = UserListDto;
 }
