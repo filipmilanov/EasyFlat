b7dd03d82c0a4b31bd891b340ce47889009193fa
Philipp Schoeller <e11907084@student.tuwien.ac.at>
2024-01-14 22:18:34 +0100

    [FV-4.5] feat: add repository method for expense search
    Refs: #200

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ExpenseRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/ExpenseRepository.java
@@ -3,13 +3,28 @@ package at.ac.tuwien.sepr.groupphase.backend.repository;
 import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
     List<Expense> findAllByPaidByIs(ApplicationUser user);
+
+    @Query("SELECT e FROM Expense e "
+        + "WHERE (:title IS NULL OR UPPER(e.title) LIKE UPPER(CONCAT('%', :title, '%')))"
+        + "AND (:paidById IS NULL OR e.paidBy.id = :paidById)"
+        + "AND ((:startOfDay IS NULL AND :endOfDay IS NULL) OR (e.createdAt BETWEEN :startOfDay AND :endOfDay))"
+        + "AND (:amountInCents IS NULL OR e.amountInCents >= :amountInCents)")
+    List<Expense> findByCriteria(@Param("title") String title,
+                                 @Param("paidById") Long paidById,
+                                 @Param("amountInCents") Double amountInCents,
+                                 @Param("startOfDay") LocalDateTime startOfDay,
+                                 @Param("endOfDay") LocalDateTime endOfDay
+    );
 
     List<Expense> findByPaidByIsIn(Set<ApplicationUser> users);
 
