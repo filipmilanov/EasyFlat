0370aade7bfbe79058ca8edf1fc2804ca1c1acc7
Ivan Andreev <e12025981@student.tuwien.ac.at>
2024-01-17 08:59:00 +0100

    [KV] only ingredients, with the same or a convertable unit are shown in the matching modal.

--- frontend/src/app/services/item.service.ts
+++ frontend/src/app/services/item.service.ts
@@ -97,9 +97,10 @@ export class ItemService {
     return this.http.delete<ItemDto>(this.baseUri + '/' + itemId);
   }
 
-  findByName(productName:string): Observable<ItemDto> {
-    console.log('Find by name: ' + productName);
-    return this.http.get<ItemDto>(this.baseUri + '/name/' + productName)
+  findByName(productName: string, unitName: string): Observable<ItemDto> {
+    let params = new HttpParams();
+    params = params.append('unitName', unitName);
+    return this.http.get<ItemDto>(this.baseUri + '/name/' + productName,{params})
   }
 
 }
