71136f734d07478b2fd2d41cd0d5203651394f2d
Alex Ofner <e12119038@student.tuwien.ac.at>
2024-01-22 13:08:42 +0100

    fix: try to delete debits manual before user delete

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/DebitRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/DebitRepository.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.sepr.groupphase.backend.repository;
 
+import at.ac.tuwien.sepr.groupphase.backend.entity.ApplicationUser;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Debit;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,8 @@ import java.util.List;
 public interface DebitRepository extends JpaRepository<Debit, Long> {
 
     List<Debit> findAllById_ExpenseIsIn(List<Expense> expenses);
+
+    List<Debit> findAllById_User(ApplicationUser user);
+
+    void deleteAllById_User_IdIs(Long user);
 }
